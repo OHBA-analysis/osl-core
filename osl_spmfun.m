303514f93d6e54720867bdc60260a3f2ecb3cb5f
Adam Baker <adambaker86@gmail.com>
2014-11-26 20:32:31 +0000

    added wrapper to call spm function without creating new file

---
+++ osl_spmfun.m
@@ -0,0 +1,27 @@
+function D = osl_spmfun(fun,S)
+% Apply SPM function to MEEG object without creating a new file
+% FORMAT D = osl_spmfun(S,fun)
+%
+% INPUTS:
+% fun         - function handle to a valid SPM function
+% S           - input structure containing fields necessary for fun
+%
+% OUTPUTS:
+% D           - MEEG object (also written to disk)
+%
+% Adam Baker 2014
+
+try 
+    D = spm_eeg_load(S.D);
+catch
+    error('S must contain S.D')
+end
+   
+Dnew = feval(fun,S);
+
+if isa(Dnew,'meeg')
+    D = move(Dnew,fullfile(D.path,D.fname));
+    Dnew.delete;
+end
+
+end
