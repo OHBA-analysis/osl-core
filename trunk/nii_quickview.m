adfa5930919604bd9b143e1381f7247ecf585fe1
adambaker86 <adambaker86@e505ee65-b614-5eab-0274-28891fa5f5a0>
2014-11-27 16:06:40 +0000

    added nii_quickview

---
+++ trunk/nii_quickview.m
@@ -0,0 +1,21 @@
+function nii_quickview(mat,spat_res,rs)
+% View nii file without creating (permanently) a new file
+% AB 2012
+
+global OSLDIR
+
+fname_tmp = tempname;
+
+nii_quicksave(mat,fname_tmp,spat_res);
+
+if exist('rs','var')
+  stdbrain = [OSLDIR '/std_masks/MNI152_T1_' num2str(rs) 'mm_brain.nii.gz'];
+  osl_resample_nii(fname_tmp, fname_tmp, rs, 'trilinear',stdbrain);
+end
+  
+fslview(fname_tmp);
+
+pause(5)
+dos(['rm ' fname_tmp '.nii.gz']);
+
+end
