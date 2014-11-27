c1afc06a9471b940addd3a6df7379fb2c11b37d4
woolrich <woolrich@fmrib.ox.ac.uk>
2014-11-27 18:50:49 +0000

    Fixed problems with modalities

--- spm-beamforming-toolbox-osl-addons/bf_write_spmeeg_osl.m
+++ spm-beamforming-toolbox-osl-addons/bf_write_spmeeg_osl.m
@@ -4,21 +4,7 @@ function res = bf_write_spmeeg_osl(BF, S)
 %
 % Adam Baker
 %--------------------------------------------------------------------------
-if nargin == 0
-    
-    modality         = cfg_menu;
-    modality.tag     = 'modality';
-    modality.name    = 'Modality';
-    modality.help    = {'What modality to output'};
-    modality.labels  = {'MEG'
-                        'MEGMAG'
-                        'MEGPLANAR'
-                        'EEG'}';
-    modality.values  = {'MEG'
-                        'MEGMAG'
-                        'MEGPLANAR'
-                        'EEG'}';
-    modality.val = {'MEG'};
+if nargin == 0        
     
     none = cfg_const;
     none.tag = 'none';
@@ -29,14 +15,13 @@ if nargin == 0
     prefix.tag     = 'prefix';
     prefix.name    = 'Filename Prefix';
     prefix.help    = {'Specify the string to be prepended to the output (if relevant).'};
-    %prefix.strtype = 's';
-    %prefix.num     = [1 Inf];
+    prefix.strtype = 's';
     prefix.val     = {''};
     
     spmeeg      = cfg_branch;
     spmeeg.tag  = 'spmeeg_osl';
     spmeeg.name = 'SPM M/EEG dataset';
-    spmeeg.val  = {modality, prefix};
+    spmeeg.val  = {prefix};
     
     res = spmeeg;
     
@@ -47,43 +32,48 @@ end
 
 D = BF.data.D;
 
-% Get original channel types and units from first montage    
-montage = BF.output.montage.(S.modality)(1);
-chantypeorg = chantype(D, D.indchannel(montage.labelorg))';
-chanunitorg = units(D, D.indchannel(montage.labelorg))';
+modalities = intersect(fieldnames(BF.features), {'EEG', 'MEG', 'MEGMAG', 'MEGGRAD', 'MEGPLANAR'});
 
-% Apply multiple virtual montages
-for m = 1:numel(BF.output.montage.(S.modality))
-    
-    montage = BF.output.montage.(S.modality)(m);
-    montage.chantypeorg = chantypeorg;
-    montage.chanunitorg = chanunitorg;
-        
-    % Online montage needs additional channel information
-    for ch = 1:length(montage.labelnew)
-        montage.channels(ch).label = montage.labelnew{ch};
-        montage.channels(ch).type  = montage.chantypenew{ch};
-        montage.channels(ch).units = montage.chanunitnew{ch};
-        montage.channels(ch).bad   = 0;
-    end 
-    
-    S1 = [];
-    S1.montage      = montage;
-    S1.keepsensors  = false;
-    S1.keepothers   = false;
-    S1.mode         = 'switch';
-   
-    % Write a new MEEG object if a prefix is given
-    if ~isempty(S.prefix) 
-        if m == 1
-            D = copy(D, [S.prefix D.fname]);
+for mm  = 1:numel(modalities)
+
+    % Get original channel types and units from first montage    
+    montage = BF.output.montage.(modalities{mm})(1);
+    chantypeorg = chantype(D, D.indchannel(montage.labelorg))';
+    chanunitorg = units(D, D.indchannel(montage.labelorg))';
+
+    % Apply multiple virtual montages
+    for m = 1:numel(BF.output.montage.(modalities{mm}))
+
+        montage = BF.output.montage.(modalities{mm})(m);
+        montage.chantypeorg = chantypeorg;
+        montage.chanunitorg = chanunitorg;
+
+        % Online montage needs additional channel information
+        for ch = 1:length(montage.labelnew)
+            montage.channels(ch).label = montage.labelnew{ch};
+            montage.channels(ch).type  = montage.chantypenew{ch};
+            montage.channels(ch).units = montage.chanunitnew{ch};
+            montage.channels(ch).bad   = 0;
+        end 
+
+        S1 = [];
+        S1.montage      = montage;
+        S1.keepsensors  = false;
+        S1.keepothers   = false;
+        S1.mode         = 'switch';
+
+        % Write a new MEEG object if a prefix is given
+        if ~isempty(S.prefix) 
+            if m == 1
+                D = copy(D, [S.prefix D.fname]);
+            end
         end
+
+        S1.D = D;
+        D = spm_eeg_montage(S1);
+        D.save;
+
     end
-    
-    S1.D = D;
-    D = spm_eeg_montage(S1);
-    D.save;
-    
-end
+end;
 
 res.files = {fullfile(D)};
