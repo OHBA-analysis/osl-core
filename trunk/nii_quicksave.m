7377be6d077e77ce603a05c5fcacfe9d69ddb840
adambaker86 <adambaker86@e505ee65-b614-5eab-0274-28891fa5f5a0>
2014-11-27 16:10:57 +0000

    restored nii_quicksave resampling fix

--- trunk/nii_quicksave.m
+++ trunk/nii_quicksave.m
@@ -1,26 +1,28 @@
-%% nii_quicksave.m
-%
-% converts from a matrix to a volume and saves a nii.gz file.
-%
-% fname_out = nii_quicksave(mat,fname,spat_res,resamp,interptype)
-% fname is output file name (inc. path).
-% spat_res in the resolution of the volume in mm.
-% mat is the 2D matrix input.
-%
-% HL 2012
-
-function fname_out = nii_quicksave(mat,fname,spat_res,resamp,interptype)
+function fname_out = nii_quicksave(mat,fname,spat_res,resamp,interp)
+% nii_quicksave.m
+%fname_out = nii_quicksave(mat,fname,spat_res,resamp)
 
 global OSLDIR;
 stdbrain=read_avw([OSLDIR '/std_masks/MNI152_T1_' num2str(spat_res) 'mm_brain.nii.gz']);
 save_avw(matrix2vols(mat,stdbrain),fname,'f',[spat_res spat_res spat_res size(mat,2)]);
-if nargin <5
-   interptype = 'trilinear';
+
+if ~exist('interp','var')
+  interp = 'trilinear';
 end
-if nargin >=4
-    fname_rs=[fname '_ds' num2str(resamp) 'mm'];
-    fname_out = osl_resample_nii(fname,fname_rs,resamp,interptype);
-else
-    fname_out=fname;
+
+fname = strrep(fname,'.gz','');
+fname = strrep(fname,'.nii','');
+
+
+if exist('resamp','var')
+  fname_rs=[fname '_ds' num2str(resamp) 'mm'];
+  stdbrain = [OSLDIR '/std_masks/MNI152_T1_' num2str(resamp) 'mm_brain.nii.gz'];
+  osl_resample_nii(fname, fname_rs, resamp,interp,stdbrain);
+  dos(['rm ' fname '.nii.gz']);
+  dos(['mv ' fname_rs '.nii.gz ' fname '.nii.gz']);
 end
+
+  fname_out=[fname '.nii.gz'];
+
+
 end
