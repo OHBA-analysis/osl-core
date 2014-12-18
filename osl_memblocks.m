ea8e5064bb693ee5aad2a6f3dff99fc7e3b7e6d4
Adam Baker <adambaker86@gmail.com>
2014-12-18 14:39:29 +0000

    added function to split large matrix into memory efficient blocks

---
+++ osl_memblocks.m
@@ -0,0 +1,20 @@
+function blks = osl_memblocks(X,dim,MaxMem)
+% Returns indices into X that splits the data into blocks no greater than 
+% MAXMEM bytes along dimension DIM
+
+if nargin < 2
+    dim = 1;
+end
+
+if nargin < 3
+    MaxMem = 200*2^20; % bytes
+end
+
+Mem_blk = 8*size(X,setxor(dim,[1,2]));
+
+blk_size = floor(MaxMem./Mem_blk);
+blks     = 0:blk_size:size(X,dim);
+blks     = unique([blks size(X,dim)]);
+blks     = [blks(1:end-1)+1; blks(2:end)]';
+
+end
