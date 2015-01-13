784cd227d824139f17f9a3d3875e57cd5d748b99
giles.colclough <giles.colclough@gmail.com>
2015-01-13 18:34:34 +0000

    Added functions necessary to run

--- osl_filelist.m
+++ osl_filelist.m
@@ -1,15 +1,35 @@
-function filelist = osl_filelist(pathstr,filestr)
-% Gets a list of files from a directory specified by pathstr that match the
-% in filestr
+function sortedList = osl_filelist(pathstr,filestr)
+%OSL_FILELIST Gets a list of files from a directory matching a pattern
 %
-% Usage:
-% filelist = osl_filelist(dirname,filestr)
+% LIST = OSL_FILELIST(DIRNAME, PATTERN) matches files with string PATTERN
+%   in directory DIRNAME, sorting in a sensible order. 
 %
 %
 % Adam Baker 2014
-    filelist = dir(fullfile(pathstr,filestr));
-    filelist = {filelist.name};
-    filelist = fullfile(pathstr,filelist);
-    filelist = sort_nat(filelist)';
+
+
+%	Copyright 2014 OHBA
+%	This program is free software: you can redistribute it and/or modify
+%	it under the terms of the GNU General Public License as published by
+%	the Free Software Foundation, either version 3 of the License, or
+%	(at your option) any later version.
+%	
+%	This program is distributed in the hope that it will be useful,
+%	but WITHOUT ANY WARRANTY; without even the implied warranty of
+%	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%	GNU General Public License for more details.
+%	
+%	You should have received a copy of the GNU General Public License
+%	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+%	$LastChangedBy$
+%	$Revision$
+%	$LastChangedDate$
+
+
+tmp        = dir(fullfile(pathstr,filestr));
+filelist   = {tmp(:).name};
+filelist   = fullfile(pathstr,filelist(:));
+sortedList = sort_nat(filelist);
     
-end
+end%osl_filelist
