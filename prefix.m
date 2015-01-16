ff2df7a4739228df33850f9da71b29ac58047482
adambaker86 <adambaker86@e505ee65-b614-5eab-0274-28891fa5f5a0>
2015-01-16 11:05:45 +0000

    added prefix.m, removed duplicated add_fname_prefix.m

---
+++ prefix.m
@@ -0,0 +1,26 @@
+function filestr = prefix(filestr, prefixstr)
+% PREFIX prefixes a character onto a set of filenames.
+% FILESTR = PREFIX(FILESTR, PREFIXSTR) adds prefix PREFIXSTR to filename(s)
+% to filenames in FILESTR. FILESTR can be a single string, or a cell
+% array of strings. PREFIXSTR is a character string.
+
+assert(ischar(prefixstr),               ...
+    [mfilename ':IncorrectPrefixType'], ...
+    'Prefix should be a string. \n');
+
+% apply prefix, catering for character or cell arrays
+if iscell(filestr)
+    for s = 1:numel(filestr)
+        [p,f,e] = fileparts(filestr{s});
+        filestr{s} = fullfile(p,[prefix,f,e]);
+    end
+elseif ischar(filestr)
+    [p,f,e] = fileparts(filestr);
+    filestr = fullfile(p,[prefix,f,e]);
+else
+    error([mfilename ':InconsistentInput'], ...
+        'Expecting a string or cell array of strings as input. \n');
+end
+
+
+end
