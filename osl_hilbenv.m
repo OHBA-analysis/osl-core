31dbcd6e86016c297e2619b81da30b1b37aa6a3c
adambaker86 <adambaker86@e505ee65-b614-5eab-0274-28891fa5f5a0>
2015-01-22 16:50:35 +0000

    removed core envelope & averaging functionality from osl_hilbenv.m into generic function hilbenv.m

--- osl_hilbenv.m
+++ osl_hilbenv.m
@@ -30,19 +30,21 @@ end
 trl = 1; % TODO - fix for trialwise data
     
 % Set up new MEEG object to hold downsampled envelope
-ds_fac = ceil(S.winsize/2);
-if ds_fac < 1
-    ds_fac = 1;
-    t_env = D.time;
-else
-    t_env = D.time(ceil(S.winsize/2):end-floor(S.winsize/2));
-    t_env = t_env(ds_fac:ds_fac:end);
-end
-
+[~,t_env] = hilbenv(D.time,D.time,S.winsize);
+% ds_fac = ceil(S.winsize/2);
+% if ds_fac < 1
+%     ds_fac = 1;
+%     t_env = D.time;
+% else
+%     t_env = D.time(ceil(S.winsize/2):end-floor(S.winsize/2));
+%     t_env = t_env(ds_fac:ds_fac:end);
+% end
 
 Denv = clone(montage(D,'switch',0),prefix(D.fnamedat, 'h'),[D.nchannels,length(t_env),D.ntrials]);
 Denv = timeonset(Denv,t_env(1));
-Denv = fsample(Denv,Denv.fsample/ds_fac);
+
+fsampleNew = 1./(diff(t_env([1,end]))/length(t_env));
+Denv = fsample(Denv,fsampleNew);
 
 % Loop over blocks and voxels and apply envelope averaging
 blks = osl_memblocks(D,1);
@@ -50,28 +52,14 @@ blks = osl_memblocks(D,1);
 ft_progress('init','eta')
 for iblk = 1:size(blks,1)
     ft_progress(iblk/size(blks,1));
-
-    % Compute Hilbert transform:
-    dat_blk = transpose(abs(hilbert(D(blks(iblk,1):blks(iblk,2),:,trl)')));
-  
-    % Apply moving average:
-    if S.winsize > 0
-        dat_blk = fftfilt(repmat(ones(S.winsize,1),1,size(dat_blk,1)),dat_blk');
-        dat_blk = dat_blk./S.winsize;
-        dat_blk = dat_blk(S.winsize:end,:);
-        dat_blk = dat_blk(1:end-rem(size(dat_blk,1),ds_fac),:);
-        
-        % Downsample
-        dat_blk = resample(dat_blk,1,ds_fac)';
-        dat_blk(dat_blk<0) = 0;
-    end
     
-    Denv(blks(iblk,1):blks(iblk,2),:) = dat_blk;
+    dat_blk = D(blks(iblk,1):blks(iblk,2),:,trl);
+    env = hilbenv(dat_blk,D.time,S.winsize);   
+    Denv(blks(iblk,1):blks(iblk,2),:) = env;
 end
 ft_progress('close');
 
 Denv.save;
-
 
 end
 
