ea3bb23ebd80f48c4e2b7dc394eb0eed431f5d41
adambaker86 <adambaker86@e505ee65-b614-5eab-0274-28891fa5f5a0>
2015-01-28 17:20:32 +0000

    replaced source space channel type VE with LFP for compatibility with spm functions

--- trunk/osl_write_online_montage.m
+++ trunk/osl_write_online_montage.m
@@ -1,5 +1,18 @@
-function D = osl_write_online_montage(D)
+function D = osl_write_online_montage(S)
 % Writes the current online montage to a new MEEG object (with prefix 'M')
+% FORMAT D = osl_write_online_montage(S)
+%
+% S                     - input structure 
+%  fields of S:
+%   S.D                 - MEEG object or filename of M/EEG mat-file with
+%                         continuous data
+%
+% Output:
+% D                     - MEEG object (also written on disk)
+%
+% Adam Baker 2015
+
+D = spm_eeg_load(S.D);
 
 S               = [];
 S.D             = fullfile(D.path,D.fname);
