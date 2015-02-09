71514331c35daa3231478857278cfd6308783a7f
woolrich <woolrich@fmrib.ox.ac.uk>
2015-02-09 16:34:57 +0000

    tweak

--- osl_montage.m
+++ osl_montage.m
@@ -3,9 +3,9 @@ function [D, montage] = osl_montage(S)
 % [D, montage] = osl_montage(S)
 %
 % Wrapper call for spm_eeg_montage that restores the parts of
-% D.inv that should still be OK (i.e. the coreg bits, but not the
-% forward model bits). This avoids the need to rerun osl_headmodel, but
-% requires osl_forwardmodel to be rerun.
+% D.inv that should still be OK (i.e. it restores the coreg bits, but 
+% not the forward model bits). This avoids the need to rerun osl_headmodel,  
+% but still requires osl_forwardmodel to be rerun.
 %
 % MWW & AB 2015
 
