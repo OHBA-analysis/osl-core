e68f7184ea61a6b3d4a77260a002cd321788c411
Adam Baker <adambaker86@gmail.com>
2015-02-16 11:50:07 +0000

    updated Diego's HMM toolbox

--- osl_hmm_toolbox/HMM-MAR/utils/gamma_kl.m
+++ osl_hmm_toolbox/HMM-MAR/utils/gamma_kl.m
@@ -1,3 +1,4 @@
+
 function [D] = gamma_kl (shape_q,shape_p,rate_q,rate_p)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
@@ -18,7 +19,7 @@ if nargin<4,
   error('Incorrect number of input arguments');
 end;
 
-D =  (shape_q - 1) * psi(shape_q) - log(scale_q) - shape_q - gammaln(shape_q) + ...
+D =  (shape_q - 1) * psi(shape_q) - log(scale_p) - shape_q - gammaln(shape_q) + ...
     gammaln(shape_p) + shape_p * log(scale_p) - (shape_p - 1) * (psi(shape_q) + log(scale_q)) + ...
     scale_q * shape_q / scale_p;
 
