8e28cfa10cedb106cdb70f6ebc4c96807501fdd6
adambaker86 <adambaker86@e505ee65-b614-5eab-0274-28891fa5f5a0>
2015-02-17 22:33:17 +0000

    RHINO - updated rhino_select

--- rhino/rhino_select.m
+++ rhino/rhino_select.m
@@ -8,7 +8,7 @@ MainFig = figure('NumberTitle',           'off'     ,...
                        
 uitools.toolbar = uitoolbar;
 rotateicon = load(fullfile(matlabroot,'toolbox','matlab','icons','rotate.mat'));
-uitools.rotate = uipushtool(uitools.toolbar,'ClickedCallback',@togglerot,'CData',rotateicon.cdata,'TooltipString','Toggle rotation');
+uitools.rotate = uitoggletool(uitools.toolbar,'ClickedCallback',@togglerot,'CData',rotateicon.cdata,'TooltipString','Toggle rotation');
 
 if any(strcmp(get(get(MainFig,'children'),'type'),'axes'))
     [az,el] = view;
@@ -65,9 +65,7 @@ set(get(hcursorNAS,'DataCursor'),'DataIndex',pointNAS,'TargetPoint',mesh.vertice
 set(get(hcursorLPA,'DataCursor'),'DataIndex',pointLPA,'TargetPoint',mesh.vertices(pointLPA,:));
 set(get(hcursorRPA,'DataCursor'),'DataIndex',pointRPA,'TargetPoint',mesh.vertices(pointRPA,:));
 
-updateDataCursors(cursorNAS)
-updateDataCursors(cursorLPA)
-updateDataCursors(cursorRPA)
+uiwait(MainFig)
 
 
     function movelight(varargin)
@@ -110,24 +108,16 @@ updateDataCursors(cursorRPA)
     function closefig(varargin)
 
         try
-        cursors = datacursormode(MainFig);
-        
-        % Massive PITA text parsing because I can't find a position property >:-(
-        cursorLabels = arrayfun(@(c) cursors.DataCursors(c).String{1},1:3,'UniformOutput',0);
-        
-        for lbl = fid_labels
-
-            xstr = cursors.DataCursors(strcmp(cursorLabels,lbl)).String{2};
-            ystr = cursors.DataCursors(strcmp(cursorLabels,lbl)).String{3};
-            zstr = cursors.DataCursors(strcmp(cursorLabels,lbl)).String{4};
+            cursors = datacursormode(MainFig);
+            cursorLabels = arrayfun(@(c) cursors.DataCursors(c).String{1},1:3,'UniformOutput',0);
             
-            fid_positions(strcmp(lbl,fid_labels),:) = [str2double(strrep(xstr,'x: ','')),...
-                                                       str2double(strrep(ystr,'y: ','')),...
-                                                       str2double(strrep(zstr,'z: ',''))];
-        end    
-
-        delete(MainFig)
-        
+            for lbl = fid_labels
+                fid_positions(strcmp(lbl,fid_labels),:) = get(get(cursors.DataCursors(strcmp(cursorLabels,lbl)), ...
+                    'DataCursor'),'Position');
+            end
+            
+            delete(MainFig)
+            
         catch
             delete(MainFig)
             error('error while closing figure')
