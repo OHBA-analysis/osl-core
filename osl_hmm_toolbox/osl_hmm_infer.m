b8cbc13add3d1527d0e36467f0ae435b9f6ad960
adambaker86 <adambaker86@e505ee65-b614-5eab-0274-28891fa5f5a0>
2015-02-19 11:17:27 +0000

    updated HMM toolbox

--- osl_hmm_toolbox/osl_hmm_infer.m
+++ osl_hmm_toolbox/osl_hmm_infer.m
@@ -81,3 +81,50 @@ end
 if nargout > 1
     stats = osl_hmm_stats(hmm);
 end
+
+
+% function hmmbox_infer
+% for n = 1:nreps
+%   
+%  % try % sometimes it fails...
+%     hmm_new                = struct('K',nstates);
+%     hmm_new                = hmminit(hmmdata,hmm_new,'full');
+%     hmm_new.train.cyc      = 200;
+%     hmm_new.obsmodel       = 'Gauss';
+%     hmm_new.train.obsupdate= ones(1,hmm_new.K);
+%     hmm_new.train.init     = 1;
+% 
+%     if constrain_mean
+%       disp('Constraining mean')
+%       for k=1:length(hmm_new.state)
+%         hmm_new.state(k).Mu = 0*hmm_new.state(k).Norm_Mu;
+%         hmm_new.state(k).constraint = 1;
+%       end
+%     else
+%       disp('Not constraining mean')
+%     end
+%     
+%     [hmm_new,FrEn_new] = hmmtrain(hmmdata,length(hmmdata),hmm_new);
+%     
+%     if FrEn_new < FrEn
+%       FrEn = FrEn_new;
+%       % copy persistent info to new hmm struct:
+%       hmm_new.FrEn = hmm.FrEn;
+%       hmm_new.stats = hmm.stats;
+%       % replace hmm struct with new
+%       hmm = hmm_new;
+%     end
+%     hmm.FrEn = [hmm.FrEn FrEn_new];
+%     
+%     if do_stats % save stats from this run
+%       hmmstats.state       = hmm_new.state; 
+%       hmmstats.transitions = hmm_new.P;
+%       hmmstats.statepath   = ABhmm_statepath(hmm_new); 
+%       hmmstats.KL          = ABhmm_kl(hmm_new);
+%       hmm.stats = [hmm.stats hmmstats];
+%     end
+% 
+%     
+%  % end
+%   disp(['Elapsed time = ' num2str(cputime)])
+% end
