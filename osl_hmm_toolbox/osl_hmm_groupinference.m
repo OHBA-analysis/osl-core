bc55ce2cdee9313424f028e2763b08c388e59c52
adambaker86 <adambaker86@e505ee65-b614-5eab-0274-28891fa5f5a0>
2015-03-16 16:10:03 +0000

    bugfix in osl_cov

--- osl_hmm_toolbox/osl_hmm_groupinference.m
+++ osl_hmm_toolbox/osl_hmm_groupinference.m
@@ -1,4 +1,4 @@
-function [HMMresults,statemaps,epoched_statepath_sub] = osl_hmm_groupinference(data_files,hmmdir,todo,options)
+function [HMMresults,statemaps] = osl_hmm_groupinference(data_files,hmmdir,todo,options)
 % Runs a group HMM analysis on amplitude envelopes of source reconstructed
 % MEG data using the SPM12 beamformer OR any arbitrary data saved in .mat
 % data files.
@@ -69,7 +69,7 @@ function [HMMresults,statemaps,epoched_statepath_sub] = osl_hmm_groupinference(d
 %
 % AB 2014
 
-% global OSLDIR
+%global OSLDIR
 
 HMMresults = [];
 statemaps  = [];
@@ -120,7 +120,7 @@ end
 filenames.concat = [filenames.concat '.mat'];
 
 if isfield(options.hmm,'filename') && ~isempty(options.hmm.filename)
-    [pathstr,filestr] = fileparts(options.hmm.filename);
+    [pathstr,filestr] = fileparts(options.hmm.filenam);
         if strcmp(pathstr,'')
             pathstr = hmmdir;
         end
@@ -131,7 +131,7 @@ end
 filenames.hmm = [filenames.hmm '.mat'];
 
 if isfield(options.output,'filename') && ~isempty(options.output.filename)
-    [pathstr,filestr] = fileparts(options.output.filename);
+    [pathstr,filestr] = fileparts(options.output.filenam);
         if strcmp(pathstr,'')
             pathstr = hmmdir;
         end
@@ -150,7 +150,6 @@ try todo.hmm      = todo.hmm;      catch, todo.hmm      = 1; end
 % Default envelope settings
 try windowsize = options.envelope.windowsize; catch, windowsize = 0.1;  end
 try multiband  = options.envelope.multiband;  catch, multiband  = [];   end
-try envelope_do  = options.envelope.do;  catch, envelope_do  = 1;   end
 
 % Default concatenation settings
 try logtrans  = options.concat.log;        catch, logtrans   = 0;  end
@@ -206,27 +205,12 @@ if todo.envelope
         D = spm_eeg_load(data_files{subnum});
         S = [];
         S.D = fullfile(D.path,D.fname);
-        %S.winsize = fix(windowsize*D.fsample);
         S.winsize = windowsize;
-        D = montage(D,'switch',2); D.save;
         
-        S2=[];
-        S2.D=S.D;
-        S2.outfile=prefix(D.fnamedat,'raw');
-        Draw=spm_eeg_copy(S2);
-
-        if envelope_do
-            Denv = osl_hilbenv(S);          
-            move(Denv,filenames.envelope{subnum});  
-            disp(['Saving envelope data for ' filestr ' to ' filenames.envelope{subnum}])
-        end;
-        
-        [pth fname ext]=fileparts(filenames.envelope{subnum});
-        newrawname=[pth '/' fname '_raw' ext];
-        move(Draw,newrawname);  
-        disp(['Saving raw data for ' filestr ' to ' newrawname])
-
-        clear D Denv Draw
+        D = osl_hilbenv(S);
+        move(D,filenames.envelope{subnum});  
+        disp(['Saving envelope data for ' filestr ' to ' filenames.envelope{subnum}])
+        clear D
     end
     % ---8<---8<---8<---8<---8<---8<---8<---8<---8<---8<---8<---8<---8<---
     
@@ -248,8 +232,8 @@ if todo.concat || (todo.infer && ~exist(filenames.concat,'file'))
     for f = 1:max(numel(multiband),1)
         
         % Load subjects and concatenate:
-        dat_concat = [];
-        subj_inds=[];
+        env_concat = [];
+        subj_inds = [];
         C = 0;
         
         for subnum = 1:length(data_files)
@@ -257,98 +241,44 @@ if todo.concat || (todo.infer && ~exist(filenames.concat,'file'))
             if ~isempty(multiband)
                 D = spm_eeg_load(strrep(filenames.envelope{subnum},'.mat',['_',strrep(num2str(multiband{f}),'  ','_') 'Hz.mat']));
             else
-                [pth fname ext]=fileparts(filenames.envelope{subnum});
-                newrawname=[pth '/' fname '_raw' ext];      
-                Draw = spm_eeg_load(newrawname);
-                
-                if envelope_do
-                    Denv = spm_eeg_load(filenames.envelope{subnum});
-                    Ddat = Denv;
-                    Dcov = Denv;
-                else
-                    Ddat = Draw;
-                    Dcov = Draw;                    
-                end;
-                clear Denv D Draw;
-                
+                D = spm_eeg_load(filenames.envelope{subnum});
             end
-          
-            % use all conditions
-            for condnum = 1:length(Ddat.condlist),
-                
-                trials = Ddat.indtrial(Ddat.condlist{condnum},'good'); 
-
-                for trl=1:length(trials),                            
-                                       
-                    tbad = all(badsamples(Ddat,':',':',trials(trl)));
-                    samples2use = find(~tbad);
-                    dat = Ddat(:,samples2use,trials(trl)); %#ok - SPM doesn't like logical indexing
-
-                    tbad = all(badsamples(Dcov,':',':',trials(trl)));
-                    samples2use = find(~tbad);
-                    cov_dat = Dcov(:,samples2use,trials(trl)); %#ok - SPM doesn't like logical indexing
-
-                    if logtrans && envelope_do
-                        dat = log10(dat);
-                    end
-                    
-                    if norm_subs
-                       
-                        if envelope_do
-                            dat = demean(dat,2)./std(dat(:));
-                            cov_dat = demean(cov_dat,2)./std(cov_dat(:));
-                            
-                            %dat = normalise(dat,2);
-                            %cov_dat = normalise(cov_dat,2);
-                        else
-                            %dat = demean(dat,2)./std(dat(:));
-                            %cov_dat = demean(cov_dat,2)./std(cov_dat(:));
-                    
-                            dat = normalise(dat,2);
-                            cov_dat = normalise(cov_dat,2);
-                            
-                        end;
-                    end
-
-                    dat_concat = [dat_concat,dat];
-
-                    subj_inds = [subj_inds,subnum*ones(1,size(cov_dat,2))];            
-                    %cond_inds = [cond_inds,condnum*ones(1,size(dat,2))];
-
-                    C = C + cov_dat*permute(cov_dat,[2,1]); 
-                end;
-            end;
+            tbad = any(badsamples(D,':',':',':'));
+            samples2use = find(~tbad);
+            env = D(:,samples2use); %#ok - SPM doesn't like logical indexing
+            if logtrans
+                env = log10(env);
+            end
+            if norm_subs
+                %env = demean(env,2)./std(env(:));
+                env = normalise(env,2);
+            end
+            
+            env_concat = [env_concat,env];
+            subj_inds = [subj_inds,subnum*ones(1,size(env,2))];
+            C = C + env*permute(env,[2,1]);
         end
         C = C ./ (length(subj_inds)-1);
-        clear cov_dat dat subj_inds;
+        clear env
+        
         
         % PCA + whitening - below is equivalent to fastica whitening code but much faster
         [allsvd,MixingMatrix] = eigdec(C,pcadim);
         if whiten
             MixingMatrix = diag(1./sqrt(allsvd)) * MixingMatrix';
         end
-        hmmdata =  (MixingMatrix * dat_concat)';
-        fsample = Ddat.fsample;
+        hmmdata =  (MixingMatrix * env_concat)';
+        fsample = D.fsample;
         
         if ~isempty(multiband)
             savestr = strrep(filenames.concat,'.mat',['_',strrep(num2str(multiband{f}),'  ','_') 'Hz.mat']);
             disp(['Saving concatenated envelope data to ' savestr])
-            save(savestr,'hmmdata','MixingMatrix','fsample')
+            save(savestr,'hmmdata','MixingMatrix','fsample','subj_inds')
         else
             disp(['Saving concatenated envelope data to ' filenames.concat])
-            save(filenames.concat,'hmmdata','MixingMatrix','fsample')
+            save(filenames.concat,'hmmdata','MixingMatrix','fsample','subj_inds')
         end
         
-        % save mixing matrix to nii
-        [pth nm ext]=fileparts(filenames.concat);
-        pc_maps=[pth '/' nm '_pc_maps'];
-        pc_maps = nii_quicksave(MixingMatrix',pc_maps,getmasksize(Dcov.nchannels),2);
-        disp(['PCA maps saved to ' pc_maps]);   
-        
-        [pth nm ext]=fileparts(filenames.concat);
-        mean_pc_maps=[pth '/' nm '_mean_pc_maps'];
-        mean_pc_maps = nii_quicksave(mean(abs(MixingMatrix),1)',mean_pc_maps,getmasksize(Dcov.nchannels),2);
-        disp(['mean of PCA maps saved to ' mean_pc_maps]);  
     end
 end
 
@@ -370,49 +300,26 @@ if todo.infer
             hmmdata = [hmmdata tmp.hmmdata];
             MixingMatrix{f} = tmp.MixingMatrix;
         end
-
+        subj_inds = tmp.subj_inds;
         fsample = tmp.fsample;
     else
         load(filenames.concat)
     end
     
-    %%%%%%%%%%%%%%%%
-    % Sort this OUT!
-    if 1
-        %tilde='/Users/woolrich';
-        %addpath(genpath([tilde '/homedir/vols_data/ctf_selfpaced_fingertap']));
-        global OSLDIR
-        rmpath(genpath(fullfile(OSLDIR,'osl2/osl_hmm_toolbox')));
-        addpath(genpath(fullfile(OSLDIR,'hmmbox_4_1')));
-
-        if ~envelope_do
-            hmm = ABhmm_infer(hmmdata,nstates,nreps,'constrain_mean');
-        else
-            hmm = ABhmm_infer(hmmdata,nstates,nreps);
-        end;
-        hmm.statepath = ABhmm_statepath(hmm);
-        addpath(genpath(OSLDIR));
-
-    else
-        global OSLDIR
-        rmpath(genpath(fullfile(OSLDIR,'osl2/hmmbox_4_1')));
-        addpath(genpath(fullfile(OSLDIR,'osl_hmm_toolbox')));
-        
-        hmm = osl_hmm_infer(hmmdata,struct('K',nstates,'order',0,'Ninits',nreps,'Hz',fsample,'zeromean',~envelope_do));
-        addpath(genpath(OSLDIR));
-    end;
-    %%%%%%%%%%%%%%%%
     
+    hmm = osl_hmm_infer(hmmdata,struct('K',nstates,'order',0,'Ninits',nreps,'Hz',fsample,'zeromean',0));
     hmm.MixingMatrix = MixingMatrix;
     hmm.fsample = fsample;
-
+    
     % Save results
-    disp(['Saving inferred HMM to ' filenames.hmm])    
-    save(filenames.hmm,'hmm');
+    disp(['Saving inferred HMM to ' filenames.hmm])
+    save(filenames.hmm,'hmm','subj_inds')
     
     HMMresults = filenames.hmm;
 
 end
+
+
 
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %                                                                         %
@@ -443,104 +350,29 @@ if todo.output
                 else
                     stat = zeros(size(hmm.MixingMatrix,2),hmm.K);
                 end
-                
-                subjstart_index=1;
-                
-                %%%% find out number of conditions
-                subnum=1;
-                if ~isempty(multiband)
-                    D = spm_eeg_load(strrep(filenames.envelope{subnum},'.mat',['_',strrep(num2str(multiband{f}),'  ','_') 'Hz.mat']));
-                else
-                    if envelope_do
-                        D = spm_eeg_load(filenames.envelope{subnum});
-                    else
-                        [pth fname ext]=fileparts(filenames.envelope{subnum});
-                        newrawname=[pth '/' fname '_raw' ext];      
-                        D = spm_eeg_load(newrawname);
-                    end;             
-                end
-                num_conds=length(D.condlist);
-                clear D;
-                %%%%%
-                
-                epoched_statepath_sub=cell(length(data_files),num_conds);
-                
                 for subnum = 1:length(data_files)
                     
-                    %try
+                    try
                         if ~isempty(multiband)
                             D = spm_eeg_load(strrep(filenames.envelope{subnum},'.mat',['_',strrep(num2str(multiband{f}),'  ','_') 'Hz.mat']));
                         else
-                            if envelope_do
-                                D = spm_eeg_load(filenames.envelope{subnum});
-                            else
-                                [pth fname ext]=fileparts(filenames.envelope{subnum});
-                                newrawname=[pth '/' fname '_raw' ext];      
-                                D = spm_eeg_load(newrawname);                
-                            end;
-                              
+                            D = spm_eeg_load(filenames.envelope{subnum});
                         end
                         disp(['Computing ' output_method ' maps for ' D.fname]);
                         
-                        dat_concat_sub=[];                                                
-                        
-                        % use all conditions
-                        trialstart_index=1;
-                        for condnum = 1:num_conds,
-                            trials = D.indtrial(D.condlist{condnum},'good'); 
-                            
-                            epoched_statepath_sub{subnum, condnum}=zeros(1,D.nsamples,length(trials));
-                                                      
-                            for trl=1:length(trials),                            
-                                tbad = all(badsamples(D,':',':',trials(trl)));
-                                
-                                samples2use = find(~tbad);
-                                dat = D(:,samples2use,trials(trl)); %#ok - SPM doesn't like logical indexing                                                                
-                                
-                                if logtrans
-                                    dat = log10(dat);
-                                end
-                                if norm_subs
-                                    if envelope_do
-                                        %dat = normalise(dat,2);                                        
-                                        dat = demean(dat,2)./std(dat(:));
-                                    else
-                                        dat = normalise(dat,2);   
-                                        %dat = demean(dat,2)./std(dat(:));
-                                    end;
-                                end
-
-                                dat_concat_sub = [dat_concat_sub,dat];                                                                    
-                                
-                                epoched_statepath_sub{subnum, condnum}(1,samples2use,trl)=hmm.statepath(trialstart_index:trialstart_index+size(dat,2)-1);
-                                trialstart_index=trialstart_index+size(dat,2);
-                                                    
-                            end;
-                        end;                    
+                        tbad = all(badsamples(D,':',':',':'));
+                        samples2use = find(~tbad);
+                        env = D(:,samples2use); %#ok - SPM doesn't like logical indexing
+                    catch
+                        error([output_method ' currently only supported for OAT results'])
+                    end
                     
-                        hmm_sub = hmm; 
-                        from=subjstart_index;
-                        to=from+(trialstart_index-1)-1;
-                        hmm_sub.statepath = hmm.statepath(from:to);                    
-                        subjstart_index=to+1;
-                        
-                        stat = stat + osl_hmm_statemaps(hmm_sub,dat_concat_sub,~envelope_do,output_method);
-                    
-                    %catch
-                    %    error([output_method ' currently only supported for OAT results'])
-                    %end
-                                
+                    hmm_sub = hmm; hmm_sub.statepath = hmm.statepath(subj_inds==subnum);
+                    stat = stat + osl_hmm_statemaps(hmm_sub,env,0,output_method);
                 end
-                
-                stat = stat ./ length(data_files);
+                stat = stat ./ length(unique(subj_inds));
                 disp(['Saving state spatial maps to ' statemaps])
                 statemaps = nii_quicksave(stat,statemaps,getmasksize(D.nchannels),2);
-               
-                % resave updated hmm
-                hmm.statemaps=statemaps;
-                hmm.epoched_statepath_sub=epoched_statepath_sub;
-                disp(['Saving updated hmm to ' filenames.hmm]);
-                save(filenames.hmm,'hmm');        
                 
             case 'cov'
                 cov_mats = osl_hmm_statemaps(hmm,[],[],'cov');
@@ -555,5 +387,4 @@ if todo.output
     end
     
 end
-
 
