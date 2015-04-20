01bb6822b227f78d66ba66e633121dafe8bdeafa
giles.colclough@gmail.com <giles.colclough@gmail.com@e505ee65-b614-5eab-0274-28891fa5f5a0>
2015-04-20 12:36:36 +0000

    util/prefix: expanded prefix function for legibility and use with directories and cell strings.

--- trunk/util/prefix.m
+++ trunk/util/prefix.m
@@ -1,27 +1,57 @@
+function newNames = prefix(oldNames, prefix)
+%PREFIX  prefixes a character onto a set of filenames. 
+% NEW = PREFIX(OLD, PRE) adds prefix PRE to filename OLD to make
+%   NEW filename. OLD can be a single string, or a cell array of strings.
+%   PRE is a character string. OLD can contain directories, these are taken
+%   care of.
+%	
 
-function filestr = prefix(filestr, prefixstr)
-% PREFIX prefixes a character onto a set of filenames.
-% FILESTR = PREFIX(FILESTR, PREFIXSTR) adds prefix PREFIXSTR to filename(s)
-% to filenames in FILESTR. FILESTR can be a single string, or a cell
-% array of strings. PREFIXSTR is a character string.
+%	Copyright 2014 OHBA
+%	This program is free software: you can redistribute it and/or modify
+%	it under the terms of the GNU General Public License as published by
+%	the Free Software Foundation, either version 3 of the License, or
+%	(at your option) any later version.
+%	
+%	This program is distributed in the hope that it will be useful,
+%	but WITHOUT ANY WARRANTY; without even the implied warranty of
+%	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%	GNU General Public License for more details.
+%	
+%	You should have received a copy of the GNU General Public License
+%	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-assert(ischar(prefixstr),               ...
-    [mfilename ':IncorrectPrefixType'], ...
-    'Prefix should be a string. \n');
 
+%	$LastChangedBy$
+%	$Revision$
+%	$LastChangedDate$
+%	Contact: giles.colclough@magd.ox.ac.uk
+%	Originally written on: GLNXA64 by Giles Colclough, 24-Oct-2014 10:44:58
+
+% input checking
+assert(ischar(prefix),                     ...
+       [mfilename ':IncorrectPrefixType'], ...
+       'Prefix should be a string. \n');
+   
 % apply prefix, catering for character or cell arrays
-if iscell(filestr)
-    for s = 1:numel(filestr)
-        [p,f,e] = fileparts(filestr{s});
-        filestr{s} = fullfile(p,[prefixstr,f,e]);
-    end
-elseif ischar(filestr)
-    [p,f,e] = fileparts(filestr);
-    filestr = fullfile(p,[prefixstr,f,e]);
-else
+if iscell(oldNames), 
+    newNames = cellfun(@(name) add_prefix(name, prefix), ...
+                       oldNames,                         ...
+                       'UniformOutput', false);
+    
+elseif ischar(oldNames),
+    newNames = add_prefix(oldNames, prefix);
+    
+else 
     error([mfilename ':InconsistentInput'], ...
-        'Expecting a string or cell array of strings as input. \n');
-end
+          'Expecting a string or cell array of strings as input. \n');
+end%if
+end%prefix
 
+function new = add_prefix(old, pre)
+%ADD_PREFIX
 
-end
+[dirName, fName, ext] = fileparts(old);
+
+new = fullfile(dirName, sprintf('%s%s%s', pre, fName, ext));
+end%add_prefix
+% [EOF]
