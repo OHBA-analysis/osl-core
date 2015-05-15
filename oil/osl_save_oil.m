88861425311dfbe02589d8ef800cd16c0577e5c0
giles.colclough <giles.colclough@gmail.com>
2015-05-15 11:54:29 +0000

    [oil] added os_save_oil

---
+++ oil/osl_save_oil.m
@@ -0,0 +1,19 @@
+function oil = osl_save_oil(oil)
+
+% [oil] = osl_save_oil(oil)
+%
+% save OIL structure to the file oil.fname=[oil.source_recon.dirname '/oil_' oil.enveloping.name '_' oil.concat_subs.name '_' oil.ica.name ]
+
+% construct name if doesn't exist
+if ~isfield(oil, 'fname') || isempty(oil.fname),
+    oil.fname = fullfile(oil.source_recon.dirname,        ...
+                         sprintf('oil-settings-%s-%s-%s', ...
+                                 oil.enveloping.name,     ...
+                                 oil.concat_subs.name,    ...
+                                 oil.ica.name),           ...
+                          'oil.mat');
+end%if
+
+% save
+ROInets.make_directory(fileparts(oil.fname));
+save(oil.fname, 'oil');
