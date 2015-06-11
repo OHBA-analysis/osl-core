b1be43eb4c3088e76967af61cc2f55ca7524f71d
woolrich <woolrich@fmrib.ox.ac.uk>
2015-06-11 11:23:13 +0000

    added spm-changes/private/spm_eeg_inv_mesh_ui.m to make sure spm coreg works with no mri specified

--- osl_startup.m
+++ osl_startup.m
@@ -114,6 +114,9 @@ targetdir{end+1}='spm12/@meeg';
 filelist{end+1}='osl2/spm-changes/private/spm_eeg_montage.m';
 targetdir{end+1}='spm12';
 
+filelist{end+1} = 'osl2/spm-changes/private/spm_eeg_inv_mesh_ui.m';
+targetdir{end+1}='spm12';
+
 for kk=1:length(filelist),
     runcmd(['cp -f ' osldir '/' filelist{kk} ' ' osldir '/' targetdir{kk}]);
 end;
