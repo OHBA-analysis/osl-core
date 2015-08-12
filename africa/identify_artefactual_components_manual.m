61519f47cefa9314eafa7b4e0dfdfecb8e2bd7dc
AQuinn501 <AQuinn501@gmail.com>
2015-08-12 17:11:04 +0000

    Can select africa artefact channel by index (eg EMG, eyetracker), manual component selection only...

--- africa/identify_artefactual_components_manual.m
+++ africa/identify_artefactual_components_manual.m
@@ -8,25 +8,27 @@ function [bad_components,fig_handles,fig_names,fig_titles] = identify_artefactua
 %   -  ident.do_mains:       compute metric based on mains frequency [0 or 1]
 %   -  ident.do_kurt:        compute metric based on extreme kurtosis [0 or 1]
 %   -  ident.do_cardiac:     compute metric based on similarity to cardiac cycle [0 or 1]
-%   -  ident.artefact_chans: compute metric based on correlation with external channels [e.g. {'ECG','EOG'}]
+%   -  ident.artefact_chans: compute metric based on correlation with external channels
+%                               can be either part of a channame or a specific
+%                               channel idx [e.g. {'ECG','EOG','312'}]
 %   -  ident.do_reject:      only compute & save metrics & topos (do not launch GUI)
 % Output:
 %   - bad_components: A list of the components identified as bad.
-% 
+%
 % Adam Baker 2014
 
 
 %% Set-Up
 
 % First check if metrics have already been computed:
-try 
+try
     metrics = S.ica_res.metrics;
 catch
     metrics = [];
 end
 
 
-if ~isfield(S,'ident'); 
+if ~isfield(S,'ident');
     error('Need to specify ident func variables');
 end
 
@@ -46,7 +48,7 @@ if isfield(S.ident,'do_mains') && S.ident.do_mains == 1;
 else
     do_mains = 0;
 end
-   
+
 if isfield(S.ident,'do_kurt') && S.ident.do_kurt == 1
     do_kurt = 1;
 else
@@ -57,8 +59,8 @@ if isfield(S.ident,'artefact_chans')
     try
        S.ident.artefact_chans = cellstr(S.ident.artefact_chans);
     catch
-        error('S.ident.artefact_chans should be strings');             
-    end    
+        error('S.ident.artefact_chans should be strings');
+    end
 else
     S.ident.artefact_chans = [];
 end
@@ -136,28 +138,28 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 if do_cardiac_autocorrelation && ~isfield(metrics,'cardiac_autocorrelation')
-    
+
   disp('No ECG artefact channels specified - detecting cardiac components without ECG')
-  
+
   bpm_range = [50 100]./60;
   maxlags = 2*D.fsample;
 
   [~,lags] = xcorr(tc(1,samples_of_interest),maxlags,'coeff');
   lags = lags./D.fsample;
   lags_range = lags>bpm_range(1) & lags<bpm_range(2);
-  
+
   ac_max = zeros(1,num_ics);
   for ic = 1:num_ics
     tc_bp = bandpass(tc(ic,samples_of_interest),[0 48],D.fsample);
     ac = xcorr(tc_bp,maxlags,'coeff');
     ac_max(ic) = max(ac(lags_range));
   end
-  
+
   metrics.cardiac_autocorrelation.value = ac_max(:);
-  
+
 elseif isfield(S.ident,'do_cardiac') && ~S.ident.do_cardiac && isfield(metrics,'cardiac_autocorrelation')
     metrics = rmfield(metrics,'cardiac_autocorrelation');
-  
+
 end
 
 
@@ -168,41 +170,49 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 if ~isempty(S.ident.artefact_chans) && ~isempty(S.ident.artefact_chans)
-    
+
     for artefact_chantype = unique(S.ident.artefact_chans)
         if ~isfield(metrics,artefact_chantype)
-                   
-            artefact_data = D(find(strcmp(D.chantype,artefact_chantype)),find(samples_of_interest));
-            artefact_data = reshape(artefact_data,size(artefact_data,1),[]);
+
+            if isempty( str2num(cell2mat( artefact_chantype ) ) )
+                % We have a string, extract relevant channels matching channel type name
+                artefact_data = D(find(strcmp(D.chantype,artefact_chantype)),find(samples_of_interest));
+                artefact_data = reshape(artefact_data,size(artefact_data,1),[]);
+                metric_name = cell2mat( artefact_chantype );
+            else
+                % We have a channel number
+                artefact_data = D(str2num(cell2mat(artefact_chantype)), find(samples_of_interest));
+                metric_name = ['Chan_' cell2mat(artefact_chantype)];
+            end
 
             % Bandpass filter
             for ac = 1:size(artefact_data,1)
                 artefact_data(ac,:) = bandpass(artefact_data(ac,:),[0.1 48],D.fsample);
             end
-            
+
             ac_corr = zeros(1,num_ics);
             for ic = 1:num_ics
                 tc_bp = bandpass(tc(ic,samples_of_interest),[0.1 48],D.fsample);
                 %ac_corr(ic) = sum(abs(corr(tc_bp',artefact_data')));
                 for ac = 1:size(artefact_data,1)
                     ac_corr(ic) = ac_corr(ic) + abs(nanmedian(osl_movcorr(tc_bp',artefact_data(ac,:)',D.fsample*10,0)));
-                end                    
+                end
             end
             ac_corr = ac_corr ./size(artefact_data,1);
 
             % output correlation and timecourse
-            metrics.(char(artefact_chantype)).('value')  = ac_corr(:);
-            metrics.(char(artefact_chantype)).timeCourse = artefact_data.';
+            metrics.(metric_name).('value')  = ac_corr(:);
+            metrics.(metric_name).timeCourse = artefact_data.';
 
         end
     end
-    
+
 end
 
 
 % Precompute topographies
 try
-    topos = S.ica_res.topos; 
+    topos = S.ica_res.topos;
 catch
     topos = [];
     h = figure('visible','off');
@@ -212,7 +222,7 @@ catch
         topos_m = component_topoplot(D,sm,modalities(m));
         topos = [topos handle2struct(topos_m)];
     end
-    
+
     close(h)
 end
 
@@ -265,7 +275,7 @@ comp2view = comp(indchantype(D,modality),:);
 if (strcmp(modality,'MEGPLANAR')) % Average gradiometers
     comp2view = sqrt(comp2view(1:2:end,:).^2 + comp2view(2:2:end,:).^2);
 end
-    
+
 if (strcmp(modality,'MEGMAG'))
     cfg.channel     = {'MEGMAG'};
     cfg.layout      = [OSLDIR '/layouts/neuromag306mag.lay'];
@@ -282,7 +292,7 @@ elseif (strcmp(modality,'EEG')),
     warning('EEG not currently supported, using development EEG layout');
     cfg.channel = {'EEG'};
     cfg.layout  = [OSLDIR '/layouts/EEG60.lay'];
-else   
+else
     error('Unsupported modality');
 end
 
