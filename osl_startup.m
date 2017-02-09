01f9df6a26c18f43f615fe32c87207a6a60be98b
Romesh Abeysuriya <romesh.abeysuriya@psych.ox.ac.uk>
2017-02-09 14:39:26 +0000

    Cleaned up startup script

--- osl_startup.m
+++ osl_startup.m
@@ -1,156 +1,147 @@
 function osl_startup( osldir )
+    % Initialize OSL
+    % osldir is the folder containing 'osl2'
+    
+    if nargin < 1 || isempty(osldir) 
+        f = fileparts(mfilename('fullpath'));
+        osldir = fileparts(f);
+    end
+    
+    setenv('OSLDIR',osldir)
 
-% osl_startup( osldir )
-%
-% SETS UP THE BASIC PATH SETTINGS
-%
-% MWW 2012
-%
-% does no path-changing if running in deployed mode (gw '13).
+    % does no path-changing if running in deployed mode (gw '13).
+    if ~isdeployed 
 
-setenv('OSLDIR',osldir)
+        % Check and remove toolboxes that are supplied internally as part of OSL
+        checklist={'fieldtrip', 'spm', 'osl', 'mne', 'netlab', 'fsl', 'fmt'};
+        oldpaths = strsplit(path,':');
+        restoredefaultpath;
 
-if ~isdeployed
+        for j = 1:length(oldpaths)
+            if strfind(oldpaths{j},matlabroot)
+                continue
+            else
+                if ~any(cellfun(@(x) ~isempty(strfind(oldpaths{j},x)),checklist))
+                    addpath(oldpaths{j});
+                else
+                    if ~strfind(oldpaths{j},'osl') % Don't warn about OSL
+                        fprintf(2,'Found and removed conflicting toolbox: %s\n',oldpaths{j});
+                    end
+                end
+            end
+        end
+        
+        % Check fsl has been set up
+        if isempty(getenv('FSLDIR'))
+            % Try and detect default location
+            if exist('/usr/local/fsl') 
+                fsldir = '/usr/local/fsl/';
+                setenv('FSLDIR',fsldir)
+                setenv('FSLOUTPUTTYPE','NIFTI_GZ')
+
+                % Add it to the shell path from within MATLAB 
+                curpath = getenv('PATH');
+                setenv('PATH',sprintf('%s:%s',fullfile(fsldir,'bin'),curpath));
+
+                error('check this is OK');
+            else
+                error('FSL is not installed properly. Perhaps check that the $FSLDIR/bin directory is in your PATH before starting Matlab. See the Prerequisites section at https://sites.google.com/site/ohbaosl/download');
+            end
+        end
+
+        % Try a dummy call to an fsl tool to make sure FSL is properly installed
+        [status,res]=dos(['fslval']);
+        if status~=1
+            error('FSL is not installed properly. Perhaps check that the $FSLDIR/bin directory is in your PATH before starting Matlab. See the Prerequisites section at https://sites.google.com/site/ohbaosl/download');
+        end;
+       
+        addpath(sprintf('%s/etc/matlab',getenv('FSLDIR')));
+       
+    end
+
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % check path for other versions of:
-    checklist={'fieldtrip', 'spm', 'osl', 'mne', 'netlab', 'fsl', 'fmt'};
-   
-    % and remove them:
-    oldpath=path;
-    indcolon=findstr(oldpath,':');
-    st=1;
-    
-    restoredefaultpath;
-    restoredpath=path;
-    %addpath(genpath(osldir));
-   
- 	% remove spm-changes dir:
-	%try rmpath([osldir '/osl2/spm-changes']); catch; end
-    
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % check fsl has been setup
-    if isempty(getenv('FSLDIR')),
-       error('The environmental variable FSLDIR is not set. Please exit Matlab, ensure FSLDIR is set and then restart Matlab. See the Prerequisites section at https://sites.google.com/site/ohbaosl/download');
-    end;
+    %% Copy changes to SPM code from osl
+    filelist={};targetdir={};
 
-    % try a dummy call to an fsl tool to make sure FSL is properly
-    % installed:
-    [status,res]=dos(['fslval']);
-    if(status~=1)
-        error('FSL is not installed properly. Perhaps check that the $FSLDIR/bin directory is in your PATH before starting Matlab. See the Prerequisites section at https://sites.google.com/site/ohbaosl/download');
-    end;
-   
-    addpath(sprintf('%s/etc/matlab',getenv('FSLDIR')));
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-       
-    jj=1;
-    found=zeros(length(checklist),1);
-   
-    for ii=1:length(indcolon),
-        pathlistitem=oldpath(st:indcolon(ii)-1);
-       
-        ok=1;
-        for kk=1:length(checklist),
-            if(any(findstr(pathlistitem,checklist{kk}))),
-                ok=0;
-                found(kk)=1;
-            end;
-        end;
-       
-        if(ok)
-            if(~any(findstr(restoredpath,pathlistitem)))
-                addpath(pathlistitem);
-            end;
-        end;
-       
-        st=indcolon(ii)+1;
-    end;
-   
-end % if ~ isdeployed
+    filelist{end+1}='osl2/spm-beamforming-toolbox-osl-addons/bf_output_montage_osl.m';
+    targetdir{end+1}='spm12/toolbox/spm-beamforming-toolbox';
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Copy changes to SPM code from osl
-filelist={};targetdir={};
+    filelist{end+1}='osl2/spm-beamforming-toolbox-osl-addons/bf_write_spmeeg_osl.m';
+    targetdir{end+1}='spm12/toolbox/spm-beamforming-toolbox';
 
-filelist{end+1}='osl2/spm-beamforming-toolbox-osl-addons/bf_output_montage_osl.m';
-targetdir{end+1}='spm12/toolbox/spm-beamforming-toolbox';
+    filelist{end+1}='osl2/spm-beamforming-toolbox-osl-addons/bf_inverse_mne_adaptive.m';
+    targetdir{end+1}='spm12/toolbox/spm-beamforming-toolbox';
 
-filelist{end+1}='osl2/spm-beamforming-toolbox-osl-addons/bf_write_spmeeg_osl.m';
-targetdir{end+1}='spm12/toolbox/spm-beamforming-toolbox';
+    filelist{end+1}='osl2/spm-changes/private/ft_read_event_4osl.m';
+    targetdir{end+1}='spm12/external/fieldtrip/fileio';
 
-filelist{end+1}='osl2/spm-beamforming-toolbox-osl-addons/bf_inverse_mne_adaptive.m';
-targetdir{end+1}='spm12/toolbox/spm-beamforming-toolbox';
+    filelist{end+1}='osl2/spm-changes/private/read_trigger_4osl.m';
+    targetdir{end+1}='spm12/external/fieldtrip/fileio/private';
 
-filelist{end+1}='osl2/spm-changes/private/ft_read_event_4osl.m';
-targetdir{end+1}='spm12/external/fieldtrip/fileio';
+    filelist{end+1}='osl2/spm-changes/private/badsamples.m';
+    targetdir{end+1}='spm12/@meeg';
 
-filelist{end+1}='osl2/spm-changes/private/read_trigger_4osl.m';
-targetdir{end+1}='spm12/external/fieldtrip/fileio/private';
+    filelist{end+1} = 'osl2/spm-changes/private/undobalancing.m';
+    targetdir{end+1}='spm12/external/fieldtrip/forward/private/';
 
-filelist{end+1}='osl2/spm-changes/private/badsamples.m';
-targetdir{end+1}='spm12/@meeg';
+    filelist{end+1} = 'osl2/spm-changes/private/undobalancing.m';
+    targetdir{end+1}='spm12/external/fieldtrip/plotting/private/';
 
-filelist{end+1} = 'osl2/spm-changes/private/undobalancing.m';
-targetdir{end+1}='spm12/external/fieldtrip/forward/private/';
+    filelist{end+1} = 'osl2/spm-changes/private/undobalancing.m';
+    targetdir{end+1}='spm12/external/fieldtrip/fileio/private/';
 
-filelist{end+1} = 'osl2/spm-changes/private/undobalancing.m';
-targetdir{end+1}='spm12/external/fieldtrip/plotting/private/';
+    filelist{end+1} = 'osl2/spm-changes/private/undobalancing.m';
+    targetdir{end+1}='spm12/external/fieldtrip/utilities/private/';
 
-filelist{end+1} = 'osl2/spm-changes/private/undobalancing.m';
-targetdir{end+1}='spm12/external/fieldtrip/fileio/private/';
+    filelist{end+1} = 'osl2/spm-changes/private/undobalancing.m';
+    targetdir{end+1}='spm12/external/fieldtrip/private/';
 
-filelist{end+1} = 'osl2/spm-changes/private/undobalancing.m';
-targetdir{end+1}='spm12/external/fieldtrip/utilities/private/';
+    filelist{end+1} = 'osl2/spm-changes/private/ft_headmodel_localspheres.m';
+    targetdir{end+1}='spm12/external/fieldtrip/forward/';
 
-filelist{end+1} = 'osl2/spm-changes/private/undobalancing.m';
-targetdir{end+1}='spm12/external/fieldtrip/private/';
+    filelist{end+1}='osl2/spm-changes/private/path.m';
+    targetdir{end+1}='spm12/@meeg';
 
-filelist{end+1} = 'osl2/spm-changes/private/ft_headmodel_localspheres.m';
-targetdir{end+1}='spm12/external/fieldtrip/forward/';
+    filelist{end+1}='osl2/spm-changes/private/spm_eeg_montage.m';
+    targetdir{end+1}='spm12';
 
-filelist{end+1}='osl2/spm-changes/private/path.m';
-targetdir{end+1}='spm12/@meeg';
+    filelist{end+1} ='osl2/spm-changes/private/spm_eeg_inv_mesh_ui.m';
+    targetdir{end+1}='spm12';
 
-filelist{end+1}='osl2/spm-changes/private/spm_eeg_montage.m';
-targetdir{end+1}='spm12';
+    filelist{end+1} ='osl2/spm-changes/private/subsref1.m';
+    targetdir{end+1}='spm12/@meeg';
 
-filelist{end+1} ='osl2/spm-changes/private/spm_eeg_inv_mesh_ui.m';
-targetdir{end+1}='spm12';
+    filelist{end+1} ='osl2/spm-changes/private/ft_getopt.c';
+    targetdir{end+1}='spm12/external/fieldtrip/src/';
 
-filelist{end+1} ='osl2/spm-changes/private/subsref.m';
-targetdir{end+1}='spm12/@meeg';
+    for k=1:length(filelist),
+        copyfile( fullfile(osldir,filelist{k}), fullfile(osldir,targetdir{k}), 'f' );
+    end
 
-filelist{end+1} ='osl2/spm-changes/private/ft_getopt.c';
-targetdir{end+1}='spm12/external/fieldtrip/src/';
+    % Add and initialize SPM
+    addpath(fullfile(osldir,'spm12'))
+    spm_get_defaults('cmdline',true);
+    spm('Defaults','EEG')
+    addpath(fullfile(osldir, 'spm12','external','fieldtrip','src'));
 
-for kk=1:length(filelist),
-    system(['cp -rf ' osldir '/' filelist{kk} ' ' osldir '/' targetdir{kk}]);
-end;
+    % Add OHBA shared libraries
+    addpath(fullfile(osldir,'osl-external'));
+    osl_external_startup
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-OSLDIR=osldir;
+    % Ensure osl2 dir gets priority in path by adding it last
+    addpath(genpath(fullfile(osldir,'osl2')))
+    addpath(osldir)
 
-% Ensure osl2 dir gets priority in path
-addpath(genpath([osldir '/osl2/']))
-addpath(genpath([osldir '/ICA_TOOLS']))
-addpath(genpath([osldir '/MEG-ROI-nets']))
-addpath(genpath([osldir '/netlab3.3']))
+    rmpath(fullfile(osldir,'osl2','spm-changes')); % These are already copied into spm
+    rmpath(fullfile(osldir,'spm12/external/fieldtrip/external')) % This folder only contains other folders
 
-% Add only top level spm folder
-addpath([osldir '/spm12/'])
+    % Remote fieldtrip substitutes for Matlab toolboxes if the toolbox is installed
+    if license('test', 'stats')
+        rmpath(fullfile(osldir,'spm12/external/fieldtrip/external/stats'))
+    end
 
-spm_get_defaults('cmdline',true);
-spm eeg;
-close all;
+    if license('test','signal')
+        rmpath(fullfile(osldir,'spm12/external/fieldtrip/external/signal'))
+    end
 
-if ~isdeployed
-    for kk=1:length(checklist),
-        if found(kk),
-            warning(['Found and removed paths that contain the string ' checklist{kk}]);
-        end;
-    end;
-end % if ~ isdeployed
-
-% need this one:
-addpath(fullfile(osldir, 'spm12/external/fieldtrip/src'));
 
