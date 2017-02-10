6588087430fe496130a3944b2c49a8da0b6da930
Romesh Abeysuriya <romesh.abeysuriya@psych.ox.ac.uk>
2017-02-10 17:25:16 +0000

    Improved usage instructions

--- osl_publish.m
+++ osl_publish.m
@@ -1,10 +1,15 @@
 function osl_publish(filename)
-	% Provide a filename to publish a specific file
-	% or leave empty to regenerate everything
+	% Make an HTML page on the OSL website
+	%
+	% INPUT - path of the file being published
+	% OUTPUT - output html will be written in osl2/docs/matlab
+	% 
+	% To complete publishing, add files via Git and push to GitHub
+
+
 	if nargin < 1 || isempty(filename) 
 		filename = fullfile(getenv('OSLDIR'),'osl2','examples','publish_example.m');
 	end
 	
-	%publish(filename,'format','html','outputDir',fullfile(getenv('OSLDIR'),'osl2','docs','_matlab'))
 	publish(filename,'stylesheet',fullfile(getenv('OSLDIR'),'osl2','docs','mxdom2simplehtml_jekyll.xsl'),'format','html','outputDir',fullfile(getenv('OSLDIR'),'osl2','docs','matlab'))
 
