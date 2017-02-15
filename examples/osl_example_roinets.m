f1f061d881b6dc6dcb6f721d7718689710a6b519
Romesh Abeysuriya <romesh.abeysuriya@psych.ox.ac.uk>
2017-02-15 15:33:02 +0000

    Working example 1

--- examples/osl_example_roinets.m
+++ examples/osl_example_roinets.m
@@ -9,19 +9,24 @@ mkdir(output_directory)
 % Copy the demo files and make a list of the D files
 subjects = 1:10;
 D_files = {};
+session_name = {};
 mkdir(fullfile(osldir,'practical','roinets_demo'));
 for j = 1:length(subjects)
-    fname = sprintf('subject_%d',j);
-    D = spm_eeg_load(fullfile(data_dir,fname));
-    D_files{j} = D.copy(fullfile(output_directory,fname));
+    session_name{j} = sprintf('subject_%d',j);
+    D = spm_eeg_load(fullfile(data_dir,session_name{j}));
+    D_files{j} = D.copy(fullfile(output_directory,session_name{j}));
+    D_files{j} = D_files{j}.montage('switch',2);
 end
 
 %% If files already exist
-% subjects = 1:10;
-% D_files = {};
-% for j = 1:length(subjects)
-%     D_files{j} = fullfile(data_dir,sprintf('subject_%d',j));
-% end
+subjects = 1:10;
+D_files = {};
+session_name = {};
+for j = 1:length(subjects)
+    session_name{j} = sprintf('subject_%d',j);
+    D_files{j} = spm_eeg_load(fullfile(data_dir,session_name{j}));
+    D_files{j} = D_files{j}.montage('switch',2);
+end
 
 
 %% 
@@ -32,18 +37,38 @@ end
 % Settings.gridStep = p.resolution;
 
 %%
-% Provide input parcellation
+% setup the ROI network settings
 Settings = struct();
-Settings.spatialBasisSet = fullfile(osldir,'parcellations','fmri_d100_parcellation_with_PCC_reduced_2mm_ss5mm_ds8mm.nii.gz');
-Settings.gridStep = p.resolution;
-Settings = ROInets.check_inputs(Settings);
-Settings.nSessions = numel(D_files);
+Settings.spatialBasisSet          = fullfile(osldir,'parcellations','fmri_d100_parcellation_with_PCC_reduced_2mm_ss5mm_ds8mm.nii.gz'); % a binary file which holds the voxel allocation for each ROI - voxels x ROIs
+Settings.gridStep                 = 8; % mm                         % resolution of source recon and nifti parcellation file
+Settings.Regularize.do            = true;                           % use regularization on partial correlation matrices using the graphical lasso. 
+Settings.Regularize.path          = logspace(-9,2,80);              % This specifies a single, or vector, of possible rho-parameters controlling the strength of regularization. 
+Settings.Regularize.method        = 'Friedman';                     % Regularization approach to take. {'Friedman' or 'Bayesian'}
+Settings.Regularize.adaptivePath  = true;                           % adapth the regularization path if necessary
+Settings.leakageCorrectionMethod  = 'closest';                      % choose from 'closest', 'symmetric', 'pairwise' or 'none'. 
+Settings.nEmpiricalSamples        = 8;                              % convert correlations to standard normal z-statistics using a simulated empirical distribution. This controls how many times we simulate data of the same size as the analysis dataset
+Settings.ARmodelOrder             = 1;                              % We tailor the empirical data to have the same temporal smoothness as the MEG data. An order of 1 should be ok.
+Settings.EnvelopeParams.windowLength = 2; % s                       % sliding window length for power envelope calculation. See Brookes 2011, 2012 and Luckhoo 2012. 
+Settings.EnvelopeParams.useFilter    = true;                        % use a more sophisticated filter than a sliding window average
+Settings.EnvelopeParams.takeLogs  = true;                           % perform analysis on logarithm of envelope. This improves normality assumption
+Settings.frequencyBands           = {[8 13], [13 30], []};          % a set of frequency bands for analysis. Set to empty to use broadband. The bandpass filtering is performed before orthogonalisation. 
+Settings.timecourseCreationMethod = 'spatialBasis';                 % 'PCA',  'peakVoxel' or 'spatialBasis'
+Settings.outputDirectory          = output_directory;                         % Set a directory for the results output
+Settings.groupStatisticsMethod    = 'fixed-effects';                % 'mixed-effects' or 'fixed-effects'
+Settings.FDRalpha                 = 0.05;                           % false determination rate significance threshold
+Settings.sessionName              = session_name; 
+Settings.SaveCorrected            = struct('timeCourses',   false, ...  % save corrected timecourses
+                                           'envelopes',     true,  ...  % save corrected power envelopes
+                                           'variances',     false);     % save mean power in each ROI before correction
+
+%%
+% Could call osl_network_analysis at this point
+ROInets.osl_network_analysis(D_files,Settings)
+
+%% 
+% Or do the same manuall
 
 
-
-assert(numel(Dlist) == Settings.nSessions, ...
-       [mfilename ':WrongNoSessions'],     ...
-       'Number of sessions should match number of D objects passed in. \n');
 
 % make results directory
 ROInets.make_directory(Settings.outputDirectory);
