de97c26fd9a0032a2a3f88a623fa4b52d9495d17
Romesh Abeysuriya <romesh.abeysuriya@psych.ox.ac.uk>
2017-03-01 10:56:22 +0000

    Role of call_fsl is to shadow FSL version, so calling it is sometimes automatic and unavoidable - disable warning as a result

--- util/call_fsl.m
+++ util/call_fsl.m
@@ -1,5 +1,8 @@
 function [status,output] = call_fsl(cmd)
-	% Deprecated, left in for compatibility
-	% Note how the order of the outputs is swapped to maintain compatibility
-	fprintf(2,'Warning - this function is deprecated, preferred usage is to call runcmd() directly\n');
+	% OSL handles FSL setup, so this function is used to override the
+	% version of call_fsl.m that comes with FSL to avoid issues related
+	% to environment variables being cleared in the Matlab session
+	%
+	% Note how the order of the outputs is swapped compared to runcmd() to maintain compatibility
+	% with other FSL matlab tools (like read_avw)
 	[output,status] = runcmd(cmd);
