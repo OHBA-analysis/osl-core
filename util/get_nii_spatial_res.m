dc630fcd37ac82ba6fe7794bee5e296d677b6125
Romesh Abeysuriya <romesh.abeysuriya@psych.ox.ac.uk>
2017-03-01 10:57:21 +0000

    Cleaned up with new runcmd()

--- util/get_nii_spatial_res.m
+++ util/get_nii_spatial_res.m
@@ -1,37 +1,26 @@
 function [ mni_res ] = get_nii_spatial_res( mask_fname )
+    % [ mni_res ] = get_nii_spatial_res( mask_fname )
+    %
+    % MWW 
+    % RA - runcmd() will gracefully display a 'command not found' error if FSLDIR is not 
+    % correctly set, and this is also checked by osl2_startup.m, so no need to check here
+    % as well
 
-% [ mni_res ] = get_nii_spatial_res( mask_fname )
-%
-% MWW 
+    res=[];
 
-res=[];
-
-res = runcmd(['fslval ' mask_fname ' pixdim1']);
-
-try
+    res = runcmd(['fslval ' mask_fname ' pixdim1']);
     mni_res(1)=str2double(res);
     assert(isfinite(mni_res(1)));
-catch
-    if(strcmp(getenv('FSLDIR'),'')),
-        error('FSLDIR environmental variable is not set, please make sure that FSL is fully installed.');
-    end;
-    
-    disp(res);
-    
-    error(['Invalid file: ' mask_fname]);
-end;
 
-res=runcmd(['fslval ' mask_fname ' pixdim2']);mni_res(2)=str2num(res);
-res=runcmd(['fslval ' mask_fname ' pixdim3']);mni_res(3)=str2num(res);
+    res=runcmd(['fslval ' mask_fname ' pixdim2']);
+    mni_res(2)=str2num(res);
 
-if(status>1)
-    disp(res);
-    error('FSL is not installed properly. Perhaps check that the $FSLDIR/bin directory is in your PATH before starting Matlab');
-end;
+    res=runcmd(['fslval ' mask_fname ' pixdim3']);
+    mni_res(3)=str2num(res);
 
-if(mni_res(1)~=mni_res(2) | mni_res(2)~=mni_res(3))
-    error('all pixel dimensions should be the same for the input nii');
-end;  
+    if(mni_res(1)~=mni_res(2) | mni_res(2)~=mni_res(3))
+        error('all pixel dimensions should be the same for the input nii');
+    end 
 
-end
+
 
