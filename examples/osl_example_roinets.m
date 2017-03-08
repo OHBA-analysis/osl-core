cd718e21de9c24f7d78bb4e2c068b52676f27a1a
Romesh Abeysuriya <romesh.abeysuriya@psych.ox.ac.uk>
2017-03-08 23:35:48 +0000

    Updated ROInets workshop tutorials

--- examples/osl_example_roinets.m
+++ examples/osl_example_roinets.m
@@ -1,151 +1,144 @@
-%% Example script for ROInets-based analysis
+%% Example of group network connectivity analysis using ROInets
+%
+% This example shows how to perform a standard connectivity analysis using ROInets.
+% Core functionality is provided by |ROInets.run_network_analysis()|. The key inputs
+% are
+%
+% * A set of beamformed SPM MEEG objects
+% * A spatial basis matrix - a matrix mapping voxels to parcels
+%
+% The analysis can be time consuming, and pre-generated results are included with the OSL
+% example data (not available through GitHub). To re-run the analysis (~15-30 minutes), set
+% |run_analysis| to |true| below
+run_analysis = false;
 
+%%
+% Either way, the spatial basis will be useful for analysis, so select it it now
 osldir = getenv('OSLDIR');
+spatial_basis_file = fullfile(osldir,'parcellations','fmri_d100_parcellation_with_PCC_reduced_2mm_ss5mm_ds8mm.nii.gz');
+
+%%
+% If we are running the analysis, we need to get a list of MEEG objects. Below, we identify
+% where the data are, and where the output should be saved to.
 data_dir = fullfile(osldir,'example_data','roinets_example');
 output_directory = fullfile(osldir,'practical','roinets_demo');
-mkdir(output_directory)
+if run_analysis
+	mkdir(output_directory)
+end
 
 %%
-% Copy the demo files and make a list of the D files
-subjects = 1:10;
-D_files = {};
-session_name = {};
-mkdir(fullfile(osldir,'practical','roinets_demo'));
-for j = 1:length(subjects)
-    session_name{j} = sprintf('subject_%d',j);
-    D = spm_eeg_load(fullfile(data_dir,session_name{j}));
-    D_files{j} = D.copy(fullfile(output_directory,session_name{j}));
-    D_files{j} = D_files{j}.montage('switch',2);
+% Next, we load the MEEG files. Typically, the source-space signals are stored as an 
+% online montage. _It is essential that this montage is the selected montage_. Therefore, 
+% make sure that the montage is switched to the correct montage before going any further.
+if run_analysis
+	subjects = 1:10;
+	D_files = {};
+	session_name = {};
+	for j = 1:length(subjects)
+	    session_name{j} = sprintf('subject_%d',j);
+	    D = spm_eeg_load(fullfile(data_dir,session_name{j}));
+	    D_files{j} = D.copy(fullfile(output_directory,session_name{j}));
+	    D_files{j} = D_files{j}.montage('switch',2);
+	end
 end
-
-%% If files already exist
-subjects = 1:10;
-D_files = {};
-session_name = {};
-for j = 1:length(subjects)
-    session_name{j} = sprintf('subject_%d',j);
-    D_files{j} = spm_eeg_load(fullfile(data_dir,session_name{j}));
-    D_files{j} = D_files{j}.montage('switch',2);
-end
-
 
 %% 
-% Load the parcellation and save a parcelflag variable
-% Settings.spatialBasisSet = fullfile(output_directory,'parcelflag.nii.gz');
-% p = parcellation('fmri_d100_parcellation_with_PCC_reduced_2mm_ss5mm_ds8mm.nii.gz');
-% p.savenii(p.to_matrix(p.binarize),Settings.spatialBasisSet);
-% Settings.gridStep = p.resolution;
-
-%%
-% setup the ROI network settings
+% The settings for |ROInets.run_network_analysis()| are passed in as a struct.
+% See |run_network_analysis.m| for a full listing of available options
+if run_analysis
 Settings = struct();
-Settings.spatialBasisSet          = fullfile(osldir,'parcellations','fmri_d100_parcellation_with_PCC_reduced_2mm_ss5mm_ds8mm.nii.gz'); % a binary file which holds the voxel allocation for each ROI - voxels x ROIs
-Settings.gridStep                 = 8; % mm                         % resolution of source recon and nifti parcellation file
-Settings.Regularize.do            = true;                           % use regularization on partial correlation matrices using the graphical lasso. 
-Settings.Regularize.path          = logspace(-9,2,80);              % This specifies a single, or vector, of possible rho-parameters controlling the strength of regularization. 
-Settings.Regularize.method        = 'Friedman';                     % Regularization approach to take. {'Friedman' or 'Bayesian'}
-Settings.Regularize.adaptivePath  = true;                           % adapth the regularization path if necessary
-Settings.leakageCorrectionMethod  = 'closest';                      % choose from 'closest', 'symmetric', 'pairwise' or 'none'. 
-Settings.nEmpiricalSamples        = 8;                              % convert correlations to standard normal z-statistics using a simulated empirical distribution. This controls how many times we simulate data of the same size as the analysis dataset
-Settings.ARmodelOrder             = 1;                              % We tailor the empirical data to have the same temporal smoothness as the MEG data. An order of 1 should be ok.
-Settings.EnvelopeParams.windowLength = 2; % s                       % sliding window length for power envelope calculation. See Brookes 2011, 2012 and Luckhoo 2012. 
-Settings.EnvelopeParams.useFilter    = true;                        % use a more sophisticated filter than a sliding window average
-Settings.EnvelopeParams.takeLogs  = true;                           % perform analysis on logarithm of envelope. This improves normality assumption
-Settings.frequencyBands           = {[8 13], [13 30], []};          % a set of frequency bands for analysis. Set to empty to use broadband. The bandpass filtering is performed before orthogonalisation. 
-Settings.timecourseCreationMethod = 'spatialBasis';                 % 'PCA',  'peakVoxel' or 'spatialBasis'
-Settings.outputDirectory          = output_directory;                         % Set a directory for the results output
-Settings.groupStatisticsMethod    = 'fixed-effects';                % 'mixed-effects' or 'fixed-effects'
-Settings.FDRalpha                 = 0.05;                           % false determination rate significance threshold
+Settings.spatialBasisSet          = spatial_basis_file 	   % a binary file which holds the voxel allocation for each ROI - voxels x ROIs
+Settings.gridStep                 = 8; % mm                % resolution of source recon and nifti parcellation file
+Settings.Regularize.do            = true;                  % use regularization on partial correlation matrices using the graphical lasso. 
+Settings.Regularize.path          = logspace(-9,2,80);     % This specifies a single, or vector, of possible rho-parameters controlling the strength of regularization. 
+Settings.Regularize.method        = 'Friedman';            % Regularization approach to take. {'Friedman' or 'Bayesian'}
+Settings.Regularize.adaptivePath  = true;                  % adapth the regularization path if necessary
+Settings.leakageCorrectionMethod  = 'closest';             % choose from 'closest', 'symmetric', 'pairwise' or 'none'. 
+Settings.nEmpiricalSamples        = 8;                     % convert correlations to standard normal z-statistics using a simulated empirical distribution, this many repetitions
+Settings.ARmodelOrder             = 1;                     % We tailor the empirical data to have the same temporal smoothness as the MEG data.
+Settings.EnvelopeParams.windowLength = 2; % s              % sliding window length for power envelope calculation. See Brookes 2011, 2012 and Luckhoo 2012. 
+Settings.EnvelopeParams.useFilter    = true;               % use a more sophisticated filter than a sliding window average
+Settings.EnvelopeParams.takeLogs  = true;                  % perform analysis on logarithm of envelope. This improves normality assumption
+Settings.frequencyBands           = {[8 13], [13 30], []}; % a set of frequency bands for filtering prior to analysis. Set to empty to use broadband
+Settings.timecourseCreationMethod = 'spatialBasis';        % 'PCA',  'peakVoxel' or 'spatialBasis'
+Settings.outputDirectory          = output_directory;      % Set a directory for the results output
+Settings.groupStatisticsMethod    = 'fixed-effects';       % 'mixed-effects' or 'fixed-effects'
+Settings.FDRalpha                 = 0.05;                  % false determination rate significance threshold
 Settings.sessionName              = session_name; 
 Settings.SaveCorrected            = struct('timeCourses',   false, ...  % save corrected timecourses
                                            'envelopes',     true,  ...  % save corrected power envelopes
                                            'variances',     false);     % save mean power in each ROI before correction
+end
 
 %%
-% Could call osl_network_analysis at this point
-ROInets.osl_network_analysis(D_files,Settings)
+% Run the network analysis, or otherwise load the precomputed results
+if run_analysis
+	correlationMats = ROInets.run_network_analysis(D_files,Settings);
+	d = load(fullfile(output_directory, 'corrected-ROI-timecourses','subject_1_13-30Hz_ROI_envelope_timecourses.mat'));
+else
+	d = load(fullfile(data_dir,'ROInetworks_correlation_mats.mat'))
+end
+
+%%
+% The outputs comprise a cell array with one struct for each frequency band being analyzed.
+% The band-specific connectivity profiles are contained in these structs
+d.correlationMats
+d.correlationMats{1}
+
+%%
+% The correlation matrices are produced for each subject, and can be averaged over to 
+% obtain group-average connectivity profiles. Due to the orthogonalization, there is
+% no raw correlation between brain regions (as expected). Note that the correlations along
+% the diagonal are suppressed by adding NaNs, to improve clarity of the plots
+
+figure
+imagesc(mean(d.correlationMats{1}.correlation,3))
+axis square
+colorbar
+title('Raw correlation')
+
+figure
+imagesc(mean(d.correlationMats{1}.envCorrelation,3)+diag(nan(38,1)))
+axis square
+colorbar
+title('Envelope correlation')
+
+%%
+% Partial correlation matrices can also be obtained using regularization
+figure
+imagesc(mean(d.correlationMats{1}.envPartialCorrelation,3)+diag(nan(38,1)))
+axis square
+colorbar
+title('Envelope partial correlation')
+
+figure
+imagesc(mean(d.correlationMats{1}.envPartialCorrelationRegularized,3)+diag(nan(38,1)))
+axis square
+colorbar
+title('Envelope partial correlation (regularized)')
 
 %% 
-% Or do the same manuall
+% The statistical significance of the connectivity profiles is obtained by 
+% converting them to z-scores by using a simulated empirical null distribution.
+% This is generated using an autoregressive model. 
+figure
+imagesc(d.correlationMats{1}.groupEnvCorrelation_z+diag(nan(38,1)))
+axis square
+colorbar
+title('Envelope correlation z-score')
 
+%%
+% Another way to display connectivity is to draw lines between brain regions. As there are
+% a very large number of connections, it can be helpful to threshold the connectivity.
+% The example below loads the parcellation into a Parcellation object (provided by OSL)
+% and uses the |plot_network()| method to display the strongest 5% of connections
+p = parcellation(spatial_basis_file); 
+p.plot_network(mean(d.correlationMats{1}.envCorrelation,3),0.95)
 
-
-% make results directory
-ROInets.make_directory(Settings.outputDirectory);
-
-% save settings
-outputDirectory = Settings.outputDirectory;
-save(fullfile(outputDirectory, 'ROInetworks_settings.mat'), 'Settings');
-
-%% Run correlation analysis on each subject
-
-fprintf('%s: Running correlation analysis. \n', mfilename);
-
-for iSession = Settings.nSessions:-1:1,
-    fprintf('\n\n%s: Individual correlation analysis for file %d out of %d\n', ...
-            mfilename, Settings.nSessions - iSession + 1, Settings.nSessions);
-
-    D                          = Dlist{iSession};
-    sessionName                = Settings.sessionName{iSession};
-    matsSaveFileName{iSession} = fullfile(outputDirectory,                                      ...
-                                          sprintf('%s_single_session_correlation_mats_tmp.mat', ...
-                                                  sessionName));
-
-    if strcmpi(Settings.paradigm, 'task'),
-        mats{iSession} = ROInets.run_individual_network_analysis_task(D,                          ...
-                                                                 Settings,                   ...
-                                                                 matsSaveFileName{iSession}, ...
-                                                                 iSession);
-    elseif strcmpi(Settings.paradigm, 'rest'),
-        mats{iSession} = ROInets.run_individual_network_analysis(D,                          ...
-                                                                 Settings,                   ...
-                                                                 matsSaveFileName{iSession}, ...
-                                                                 iSession);
-    else
-        error([mfilename ':BadParadigm'], ...
-              'Unrecognised paradigm %s. \n', Settings.paradigm);
-    end%if
-end%for
-
-% reformat results - correlationMats is a cell array of frequency bands
-correlationMats = ROInets.reformat_results(mats, Settings);
-
-% save current results: will write over later
-% just in case of crash at group level
-saveFileName = fullfile(outputDirectory, 'ROInetworks_correlation_mats.mat');
-save(saveFileName, 'correlationMats');
-clear mats
-
-%% Subject-level analysis to average over sessions in a fixed-effects manner
-correlationMats = ROInets.do_subject_level_glm(correlationMats, Settings);
-
-%% Group-level analysis
-% Find whole group means
-if strcmpi(Settings.paradigm, 'rest'),
-    correlationMats = ROInets.do_group_level_statistics(correlationMats, Settings);
-end%if
-
-% Perform group-level GLM
-if ~isempty(Settings.GroupLevel),
-    correlationMats = ROInets.do_group_level_glm(correlationMats, Settings);
-end%if
-
-%% save matrices
-fprintf('\n%s: Saving Results. \n', mfilename);
-
-% save collected results
-save(saveFileName, 'correlationMats');
-
-% we stored individual results as we went along, in case of crash. Delete
-% them if we've safely got to this stage. 
-for iSession = 1:length(matsSaveFileName),
-    delete(matsSaveFileName{iSession});
-end%for
- 
-% tidy output of funciton
-Settings.correlationMatsFile = saveFileName;
-save(fullfile(outputDirectory, 'ROInetworks_settings.mat'), 'Settings');
-
-fprintf('%s: Analysis complete. \n\n\n', mfilename);
-end%osl_network_analysis
-% [EOF]
+%%
+% Another option for displaying connectivity is to display components of the connectivity
+% as an activation map. For example, performing an eigenvalue decomposition of the 
+% connectivity matrix, and then rendering spatial maps of each of the states.
+% This functionality is also provided by the Parcellation object.
+[a,b] = eig(mean(d.correlationMats{1}.envCorrelation,3));
+p.plot_activation(a(:,1));
---
+++ examples/osl_example_roinets_synthetic.m
@@ -0,0 +1,58 @@
+%% Introduction to orthogonalization
+% Spatial leakage in the beamformer results in correlations between adjacent/nearby voxels being
+% introduced, that are not the result of correlations in neural activity. As an approximation, this
+% can be corrected for by removing all zero-lag correlations. This does not eliminate correlations 
+% in the amplitude envelope, which still provides a meaningful measure of functional connectivity.
+% 
+% To demonstrate orthogonalization in practice, this example illustrates the use of the low-level
+% |ROInets| function |remove_source_leakage()| which implements the core orthogonalization algorithms.
+%
+% First, we generate a synthetic signal and plot it
+Fs       = 100; %Hz
+duration = 60; %s
+time     = 0:1.0/Fs:duration;
+nSamples = length(time);
+b        = fir1(1024, 0.5);
+nVoxels  = 3;
+[ARfilterTerms, ARnoiseVar] = lpc(b, 7); 
+% Generate data from a covariance matrix and smooth
+C    = [1  -0.1 0.6
+       -0.1   1 0.3
+        0.6 0.3   1] * ARnoiseVar;
+u    = chol(C)' * randn(nVoxels, nSamples);
+data = filter(1, ARfilterTerms, u.').';
+figure('Name', 'Input data', 'Color', 'w');
+plot(time.', data.');
+xlabel('Time')
+ylabel('Signal value')
+%%
+% Our signal has 3 'voxels' and we will assign each to its own 'parcel'
+spatialBasis = eye(3);
+
+%% 
+% Notice that the signal has correlations between channels
+corr(data')
+
+%%
+% The |remove_source_leakage()| function takes in the data matrix, as well as the orthogonalization
+% method. To apply symmetric orthogonalization, we can simply call:
+data_orthog = ROInets.remove_source_leakage(data, 'symmetric');
+
+%%
+% Now, the channels have no static zero-lag correlation
+corr(data_orthog')
+
+%%
+% However, the amplitude of the orthogonalized signals is quite different to the original signals
+std(data')
+std(data_orthog')
+
+%%
+% The 'closest' orthogonalization method also results in orthogonal signals, but iteratively 
+% rotates and scales the basis vectors to best preserve the original signals. As a result, 
+% the orthogonalized signals are still uncorrelated, but their amplitudes much better match the
+% original signals
+data_orthog_closest = ROInets.remove_source_leakage(data, 'closest');
+corr(data_orthog_closest')
+std(data_orthog_closest')
+
