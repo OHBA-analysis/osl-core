cc55e22b673eb3a439a7701123ed2b9e59086161
Romesh Abeysuriya <romesh.abeysuriya@psych.ox.ac.uk>
2017-03-20 09:43:00 +0000

    Remove caret

--- util/workbench_initialize.m
+++ util/workbench_initialize.m
@@ -11,15 +11,13 @@ function workbench_initialize(conf_file)
 	if ~exist(conf_file) % If conf file doesn't exist, write default locations AND use them immediately if they exist
 		[workbenchdir,caretdir] = guess_workbenchdir();
 		f = fopen(conf_file,'w');
-		fprintf(f,'# Specify location of WORKBENCH and CARET here\n');
+		fprintf(f,'# Specify location of WORKBENCH here\n');
 		fprintf(f,'WORKBENCH=%s\n',workbenchdir);
-		fprintf(f,'CARET=%s\n',caretdir);
 		fclose(f);
 	else % Read conf file
 		f = fopen(conf_file,'r');
 		fgetl(f); % Skip header
 		workbenchdir = fgetl(f); workbenchdir = regexp(workbenchdir,'.*= *','split'); workbenchdir = workbenchdir{2};
-		caretdir = fgetl(f); caretdir = regexp(caretdir,'.*= *','split'); caretdir = caretdir{2};
 		fclose(f);
 	end
 
@@ -27,18 +25,12 @@ function workbench_initialize(conf_file)
 		fprintf(2,sprintf('Workbench is not present. Edit the contents of %s to specify the location of Workbench\n',conf_file))
 	end
 
-	if ~exist(caretdir)
-		fprintf(2,sprintf('Caret is not present. Edit the contents of %s to specify the location of Caret\n',conf_file))
-	end
-
 	workbench_path = fullfile(workbenchdir,'bin_macosx64');
-	caret_path = fullfile(caretdir,'bin_macosx64');
 
-	setenv( 'PATH', strjoin([ {workbench_path, caret_path}, strsplit(getenv('PATH'),pathsep) ],pathsep) );
+	setenv( 'PATH', strjoin([ {workbench_path}, strsplit(getenv('PATH'),pathsep) ],pathsep) );
 
 function [workbenchdir,caretdir] = guess_workbenchdir()
 	workbenchdir = '/Applications/workbench/';
-	caretdir = '/Applications/caret/';
 
 
 
