59817c7233f1bc8ed2456c516a35f552ceeab540
Romesh Abeysuriya <romesh.abeysuriya@psych.ox.ac.uk>
2017-03-21 12:00:10 +0000

    Added tutorial text

--- examples/osl_example_workbench.m
+++ examples/osl_example_workbench.m
@@ -1,20 +1,42 @@
+%% Example of using Workbench to plot surface data
+%
+% This example shows how to render surface data from volume data using OSL and 
+% workbench. First, we need some volume data to plot. For this example, we will
+% calculate the alpha power using a parcellation. First, load an SPM object
+% and a parcellation
+
 D = spm_eeg_load(fullfile(osldir,'example_data','roinets_example','subject_1.mat'));
-D = D.montage('switch',2);
 p = parcellation(fullfile(osldir,'parcellations','fmri_d100_parcellation_with_PCC_reduced_2mm_ss5mm_ds8mm.nii.gz'));
+
+%%
+% Then, use ROInets to compute parcel timecourses from the data, including
+% leakage correction. Don't forget to select the correct montage first
+D = D.montage('switch',2);
 D = ROInets.get_node_tcs(D,p.parcelflag(true),'PCA');
 D = ROInets.remove_source_leakage(D,'symmetric');
+
+%% 
+% For each parcel, compute the power spectrum 
 V = D(:,:,:).';
-figure
-plot(V)
 for j = 1:size(V,2)
 	[spec_power(:,j),f] = pwelch(detrend(V(:,j)),10*D.fsample,[],[],D.fsample);
 end
-figure
-loglog(f,spec_power)
+
+%%
+% Now compute the power in the alpha band for each parcel
 P_alpha = trapz(f(f>=8&f<=12),spec_power(f>=8&f<=12,:))
 
+%%
+% This parcel-wise data can be expanded into a volume, and saved to a .nii file
 p.savenii(p.to_vol(P_alpha),'alpha_power');
-osl_render4D('alpha_power.nii.gz','savedir','test_4d')
 
-% Or in a single step in the current directory
-osl_render4D(p.savenii(p.to_vol(P_alpha),'alpha_power'))
+%%
+% Then we can display the .nii file using osl_render4D
+osl_render4D('alpha_power.nii.gz')
+
+%%
+% This command will write .gii and CIFTI files from the .nii file, and will open
+% them using Workbench. The .gii and CIFTI files are saved in the current directory
+% by default. You can optionally specify a directory to put them in
+
+% osl_render4D('alpha_power.nii.gz','savedir','test_4d') 
