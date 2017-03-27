4692908d37940ef1105e112656c4eb81e7a3e75e
Romesh Abeysuriya <romesh.abeysuriya@psych.ox.ac.uk>
2017-03-27 09:36:56 +0100

    Use osl_load_nii to read xform

--- osl_mnimask2mnicoords.m
+++ osl_mnimask2mnicoords.m
@@ -1,24 +1,16 @@
-function [ mni_coords xform ] = osl_mnimask2mnicoords(mask_fname)
+function [ mni_coords xform ] = osl_mnimask2mnicoords(fname)
 	% [ mni_coords xform ] = osl_mnimask2mnicoords(mask)
 	%
-	% converts an MNI standard brain mask, mask, into a list of mni_coords.
+	% converts an MNI standard brain mask into a list of mni_coords.
 	% 
 	% INPUTS
-	% - mask_fname - Name of a nii file with volume data. Spatial resolution stored in header
+	% - fname - Name of a nii file with volume data. Spatial resolution stored in header
 	% - xstart - Optionally specify the MNI coordinates of first entry in each dimension
 	%
 	% Romesh Abeysuriya 2017
 	% MW (pre-2014)
-
-	% Extract the xform matrix from the nii header
-	xform = runcmd('fslorient -getsform %s',mask_fname);
-	xform = reshape(str2num(xform),4,4)';
-
-	% Read in the mask
-	mask = read_avw(mask_fname);
 	
-	% Convert nonzero entries in mask to array indices
-	[x,y,z] = ind2sub(size(mask),find(mask));
-
-	% Use xform to convert indices to the MNI coordinates
-	mni_coords = [(x-1)*xform(1,1)+xform(1,4) (y-1)*xform(2,2)+xform(2,4) (z-1)*xform(3,3)+xform(3,4)];
+	[vol,res,xform] = osl_load_nii(fname); 
+	[x,y,z] = ind2sub(size(vol),find(vol));	% Convert nonzero entries in mask to array indices
+	mni_coords = xform*[(x-1)';(y-1)';(z-1)';ones(size(x'))]; % Use xform to convert indices to the MNI coordinates
+	mni_coords = mni_coords(1:3,:)';
