584a4713589b176e2053ca74f5a7e28fb64f6c03
Romesh Abeysuriya <romesh.abeysuriya@psych.ox.ac.uk>
2017-03-28 12:01:08 +0100

    Update variable name to reflect broader usage

--- util/add_montage.m
+++ util/add_montage.m
@@ -1,4 +1,4 @@
-function D = add_montage(D,W,name,ROIlabels)
+function D = add_montage(D,W,name,labels)
 	% Add an online montage to an MEEG object (in memory)
 	% 
 	% INPUTS
@@ -6,10 +6,10 @@ function D = add_montage(D,W,name,ROIlabels)
 	% - W (transformation matrix being applied to current montage (n_new_channels x n_old_channels)
 	% 	  This matrix premultiplies the active montage and must therefore have the same number 
 	% - name (optional string giving name of new montage)
-	% - ROIlabels (optional cell array of names for each channel)
+	% - labels (optional cell array of names for each channel)
 	
-	if nargin < 4 || isempty(ROIlabels) 
-		ROIlabels = [];
+	if nargin < 4 || isempty(labels) 
+		labels = [];
 	end
 	
 	if nargin < 3 || isempty(name) 
@@ -24,12 +24,12 @@ function D = add_montage(D,W,name,ROIlabels)
 		fprintf(2,'Warning - input transformation matrix is being transposed. It was %d x %d, and the MEEG montage has %d channels. To avoid this message, transpose the input to add_montage()\n',size(W,1),size(W,2),size(currentMontage.tra,1));
     end
     
-    if nargin < 4 || isempty(ROIlabels) 
-        ROIlabels = arrayfun(@(x) sprintf('ROI%d',x),1:size(W,1),'UniformOutput',false);
+    if nargin < 4 || isempty(labels) 
+        labels = arrayfun(@(x) sprintf('ROI%d',x),1:size(W,1),'UniformOutput',false);
 	end
 
 	assert(size(W,2)==size(currentMontage.tra,1),sprintf('Transformation matrix is wrong size (is %dx%d, needs to be Nx%d)',size(W,1),size(W,2),size(currentMontage.tra,1)))
-	assert(iscell(ROIlabels) && length(ROIlabels)==size(W,1),sprintf('ROI labels must be a cell array with %d elements',size(W,1)));
+	assert(iscell(labels) && length(labels)==size(W,1),sprintf('ROI labels must be a cell array with %d elements',size(W,1)));
 
 	newMontage      = currentMontage;
     newMontage      = rmfield(newMontage, 'channels');
@@ -43,8 +43,8 @@ function D = add_montage(D,W,name,ROIlabels)
 		newMontage.tra  = W * currentMontage.tra;
 	end
 
-	if ~isempty(ROIlabels)
-		newMontage.labelnew = ROIlabels;
+	if ~isempty(labels)
+		newMontage.labelnew = labels;
     end
 
 	D = D.montage('add', newMontage);
