7e5f5fff00f0a3978a25c9982f5a677294c66259
Diego Vidaurre <dvidaurre@gmail.com>
2017-04-04 18:02:11 +0200

    Update glean and hmmmar tutorials

---
+++ examples/osl_example_glean.m
@@ -0,0 +1,100 @@
+% GLEAN DEMO SCRIPT FOR INFERRING A GROUP HMM FROM SOURCE SPACE MEG DATA
+% Adam Baker, Jan 2016
+% Diego Vidaurre, Feb 2017
+   
+% Directory of the data
+data_dir = fullfile(osldir,'example_data','glean_example');
+% Name for this GLEAN analysis:
+glean_name = fullfile(osldir,'example_data','glean_example','hmmmar_demo.mat');
+
+% List of data files. These data consist of 2 sessions of 10 minutes of 
+% eyes open resting state data recorded from 6 subjects on an Elekta
+% Neuromag system (using the 204 planar gradiometers only). The data were
+% band-pass filtered between 4 and 30 Hz and projected onto a regular 8 mm
+% grid in source space using an LCMV beamformer. The data are saved as
+% SPM12 MEEG objects in sensor space, and the beamformer weights are saved
+% as a virtual montage.
+data = {'/data/s01_rest1.mat'
+        '/data/s01_rest2.mat'
+        '/data/s02_rest1.mat'
+        '/data/s02_rest2.mat'
+        '/data/s03_rest1.mat'
+        '/data/s03_rest2.mat'
+        '/data/s04_rest1.mat'
+        '/data/s04_rest2.mat'
+        '/data/s05_rest1.mat'
+        '/data/s05_rest2.mat'
+        '/data/s06_rest1.mat'
+        '/data/s06_rest2.mat'};
+data = fullfile(data_dir,data);
+
+% Clear settings
+settings = struct;
+
+% Envelope settings:
+settings.envelope.overwrite = 0;
+settings.envelope.log       = 0;
+settings.envelope.fsample   = 20;
+settings.envelope.mask      = fullfile(data_dir,'MNI152_T1_8mm_brain.nii.gz');
+
+% Subspace settings:
+settings.subspace.overwrite                         = 0;
+settings.subspace.normalisation                     = 'none';
+settings.subspace.parcellation.file                 = fullfile(data_dir,'fMRI_parcellation_ds8mm.nii.gz');
+settings.subspace.parcellation.orthogonalisation    = 'symmetric';
+settings.subspace.parcellation.method               = 'spatialBasis';
+
+% Model settings:
+settings.model.overwrite   = 0;
+settings.model.hmm.nstates = 8;
+settings.model.hmm.nreps   = 1;
+
+% Set up the GLEAN settings, data paths etc:
+GLEAN = glean.setup(glean_name,data,settings);
+
+% Run the analysis:
+glean.run(GLEAN)
+
+%% Load results and visualise them 
+
+load(glean_name)
+
+% show the state time courses
+glean.plot_timecourse(GLEAN)
+
+% compute temporal properties related to the estimation
+settings = struct('plot',1);
+GLEAN = glean.temporal_stats(GLEAN,settings);
+
+% display the "temporal_stats"
+GLEAN.results.temporal_stats
+% and Fractional occupancy within it
+GLEAN.results.temporal_stats.FractionalOccupancy
+
+% open the plots for this statistic
+open(GLEAN.results.temporal_stats.FractionalOccupancy.plots.stats)
+
+
+%% Creating the spatial maps for each state 
+
+% The function "glean.pcorr" will create spatial maps for each state, 
+% by computing the partial correlation between each session's state time 
+% courses and the envelope data at each voxel. These maps may be output 
+% as .nii files (or alternatively as .mat files), and may be computed using 
+% the voxelwise or parcelwise data:
+
+settings            = [];
+settings.format     = 'nii';
+settings.space      = {'parcel'};
+GLEAN = glean.pcorr(GLEAN,settings);
+
+
+% GLEAN now contains a new field "temporal_stats" within GLEAN.results: 
+GLEAN.results.pcorr
+
+% This results field contains the settings, as well as spatial maps in 
+% each subspace (voxel/parcel) for each session and the group average:
+GLEAN.results.pcorr.parcel
+
+fslview(GLEAN.results.pcorr.parcel.sessionmaps{1},[])
+
--- examples/osl_example_hmmmar.m
+++ examples/osl_example_hmmmar.m
@@ -1,22 +1,20 @@
-%% HMM-MAR 1 - Example usage
-%
 % HMM-MAR DEMO SCRIPT FOR INFERRING A GROUP SPECTRALLY DEFINED 
 % HMM FROM SOURCE SPACE MEG DATA
-%
 % This follows the paper: Vidaurre et al, NeuroImage (2016)
-%
 % Diego Vidaurre, Feb 2017
-%%
-% Initial setup - directory of the data and name of saved HMM-MAR analysis:
+   
+% Directory of the data:
 data_dir = fullfile(osldir,'example_data','hmmmar_example');
+% Name for this HMM-MAR analysis:
 hmmmar_name = fullfile(osldir,'example_data','hmmmar_example','hmmmar_demo.mat');
 
-%%
 % Set |do_analysis=1| to re-run the analysis, otherwise use precomputed result
 do_analysis = 0; 
 
-%% HMM-MAR on hilbert envelopes 
-% As in Baker et al, Elife (2014). Start by loading and preparing the data
+%% 1) HMM-MAR on hilbert envelopes 
+% Such as in Baker et al, Elife (2014)
+%% Loading and preparing the data
+
 % We are going to concatenate data into a single matrix
 % (if data is too big, the file names can also be provided to the hmmmar function)
 X = []; % data, time by regions
@@ -26,7 +24,7 @@ subjects = [1 2 4 5 6 7 8 9]; % index of the subjects
 N = length(subjects);
 
 for j = subjects % iterate through subjects
-    file = fullfile(data_dir,['sub' num2str(j) '.mat']);
+    file = [data_dir 'sub' num2str(j) '.mat'];
     load(file); % load the data
     sourcedata = sourcedata' ; % we need it (time by channels)
     T = [T size(sourcedata,1)]; % time length of this subject
@@ -37,24 +35,27 @@ for j = subjects % iterate through subjects
     X = [X; sourcedata]; % concat subject
 end
 
-%%
-% Prepare the HMM options and run the HMMMAR
+%% Prepare the HMM options and run the HMMMAR
     
 options = struct();
 options.K = 3; % number of states
 options.order =  0; % order 0 means a Gaussian distribution (adequate for power time series)
-options.covtype = 'full'; % model connectivity
-options.zeromean = 0; % model the mean, i.e. model the "amount of power" 
+options.covtype = 'full'; 
+% model connectivity, covtype='full' means that we model the covariance between regions
+options.zeromean = 0; 
+% zeromean=0 means that we model the mean, i.e. model the "amount of power"
+% zeromean=1 means that we do *not* model the mean
 options.cyc = 100;  
 options.initcyc = 10;  
 options.initrep = 3;  
 options.verbose = 1; % show progress?
 if do_analysis
     [hmm_env,Gamma_env] = hmmmar(X,T,options);
+else
+    load(hmmmar_name,'hmm_env','Gamma_env')
 end
 
-%%
-% Compute the spectral information of the states (power, coherence, etc) 
+%% Compute the spectral information of the states (power, coherence, etc) 
 % using a weighted version of the multitaper
 % Note that we can't get an estimation of the spectra directly from the
 % parameters in this case, as the HMM has been run in wideband power time series
@@ -67,11 +68,14 @@ options.win = 10 * options.Fs; % window, related to the level of detail in frequ
 
 if do_analysis
     spectra_env = hmmspectramt(X,T,Gamma_env,options);
+else
+    load(hmmmar_name,'spectra_env')
 end
 
-%% HMM-MAR on raw signals 
-% As in Vidaurre et al, NeuroImage (2016)
-% Loading and preparing the data
+%% 2) HMM-MAR on raw signals 
+% Such as in Vidaurre et al, NeuroImage (2016)
+%% Loading and preparing the data
+
 % We are going to concatenate data into a single matrix
 % (if data is too big, the file names can also be provided to the hmmmar function)
 X = []; % data, time by regions
@@ -81,7 +85,7 @@ subjects = [1 2 4 5 6 7 8 9]; % index of the subjects
 N = length(subjects);
 
 for j = subjects % iterate through subjects
-    file = fullfile(data_dir,['sub' num2str(j) '.mat']);
+    file = [data_dir 'sub' num2str(j) '.mat'];
     load(file); % load the data
     sourcedata = sourcedata' ; % we need it (time by channels)
     T = [T size(sourcedata,1)]; % time length of this subject
@@ -89,20 +93,17 @@ for j = subjects % iterate through subjects
     X = [X; sourcedata]; % concat subject
 end
 
-%%
-% Signs might be arbitrarily flipped, so we need to disambiguate this
+%% Signs might be arbitrarily flipped, so we need to disambiguate this
 % this is because the intrinsic ambiguity in source-reconstructed signals
 
 options = struct();
 options.maxlag = 8;
 options.noruns = 100;
-options.verbose = 0;
 
 [flips,scorepath] = findflip(X,T,options);
 X = flipdata(X,T,flips);
 
-%%
-% And run the HMM-MAR on this
+%% And run the HMM-MAR on this
 
 options = struct();
 options.K = 3; % number of states
@@ -114,10 +115,11 @@ options.initrep = 3;
 options.verbose = 1; % show progress?
 if do_analysis
     [hmm_raw,Gamma_raw] = hmmmar(X,T,options);
+else
+    load(hmmmar_name,'hmm_raw','Gamma_raw')
 end
 
-%%
-% Compute the spectral information of the states (power, coherence, etc) 
+%% Compute the spectral information of the states (power, coherence, etc) 
 % Because a MAR model implicitly contains all the spectral information, 
 % we can use the parameters to derive power, coherence, phase relations, etc.
 
@@ -129,17 +131,19 @@ options.order = 11;
 
 if do_analysis
     spectra_raw = hmmspectramar(X,T,[],Gamma_raw,options);
+else
+    load(hmmmar_name,'spectra_raw')
 end
 
-%% Analyzing HMM output
+%% Now we do some plotting of the HMM on power envelopes and HMM-MAR results
+% computed above
+
 % Because the number of cycles was set to such a low number, 
-% we reload a previously computed run, which would have taken a bit longer
+% we reload a pre-computed run, which would have taken a bit longer
 
 load(hmmmar_name)
 
-
-%%
-% Now let's see the state evoked probability, locked to the stimulus
+%% Now let's see the state evoked probability, locked to the stimulus
 % the stimulus is saved in the variable onset, which contains a (Tx1)
 % logical vector with 1 when the fingertapping is effected. 
 
@@ -148,12 +152,11 @@ Hz = 200; % sampling frequency
 L = 8; % length of the window around the stimulus (seconds)
 window = Hz*L+1;
 
-%%
 % "evoked state response" around the stimulus, for the envelope run
 evokedGamma_env = zeros(window,hmm_env.K,length(subjects));
 t0 = 0;
 for j = subjects % iterate through subjects
-    file = fullfile(data_dir,['sub' num2str(j) '.mat']);
+    file = [data_dir 'sub' num2str(j) '.mat'];
     load(file,'onset'); % load the data
     T = length(onset);
     index = t0 + (1:T);
@@ -163,12 +166,11 @@ for j = subjects % iterate through subjects
 end
 evokedGamma_env = mean(evokedGamma_env,3); % average across subjects
 
-%%
 % "evoked state response" around the stimulus, for the envelope run
 evokedGamma_raw = zeros(window,hmm_env.K,length(subjects));
 t0 = 0;
 for j = subjects % iterate through subjects
-    file = fullfile(data_dir,['sub' num2str(j) '.mat']);
+    file = [data_dir 'sub' num2str(j) '.mat'];
     load(file,'onset'); % load the data
     T = length(onset);
     index = t0 + (1:T-hmm_raw.train.order);
@@ -178,16 +180,17 @@ for j = subjects % iterate through subjects
 end
 evokedGamma_raw = mean(evokedGamma_raw,3); % average across subjects
  
-%%
 % And plot both 
 figure(1);
 halfwindow = (window-1)/2;
+% Evoked state response for the HMM on power envelopes
 subplot(1,2,1)
 plot((-halfwindow:halfwindow)/Hz,evokedGamma_env,'LineWidth',2)
 xlabel('Time (s)','FontSize',15)
 ylabel('State probability','FontSize',15)
 title('HMM on envelope','FontSize',17)
 ylim([0.05 0.7])
+% Evoked state response for the HMM-MAR
 subplot(1,2,2)
 plot((-halfwindow:halfwindow)/Hz,evokedGamma_raw,'LineWidth',2)
 xlabel('Time (s)','FontSize',15)
@@ -195,12 +198,13 @@ ylabel('State probability','FontSize',15)
 title('HMM-MAR on raw signals','FontSize',17)
 ylim([0.05 0.7])
 
-%%
-% Show the spectral info (power and coherence) for the MAR
+%% Show the spectral info (power and coherence) for the 
+% HMM on power envelopes and HMM-MAR
 
 colors = {'b',[0.2 0.7 0.2],'r'};
 
-figure(2);clf(2) % parametric spectra, from the HMM-MAR run
+% Non-parametric spectra from the HMM on power envelopes
+figure(2);clf(2)  
 for k=1:hmm_env.K
     subplot(2,2,1)
     hold on
@@ -225,8 +229,8 @@ title('Power Chan2, HMM-Gaussian','FontSize',16)
 subplot(2,2,3)
 title('Coherence, HMM-Gaussian','FontSize',16)
 
-
-figure(4);clf(4) % parametric spectra, from the HMM-MAR run
+% Parametric spectra (MAR-based) from the HMM-MAR
+figure(3);clf(3)  
 for k=1:hmm_env.K
     subplot(2,2,1)
     hold on
@@ -252,47 +256,76 @@ title('Power Chan2, HMM-MAR','FontSize',16)
 subplot(2,2,3)
 title('Coherence, HMM-MAR','FontSize',16)
 
-%%
-% HMM-based Time-frequency analysis
+
+%% HMM-based Time-frequency analysis
+% Given the higher sensitivity of the HMM-MAR spectra, apparent in the
+% previous block, we focus here just on the HMM-MAR results 
 
 [psd_tf,coh_tf] = hmmtimefreq(spectra_raw,evokedGamma_raw,1);
 f = spectra_raw.state(1).f ;
 indf = f>4 & f<=30;
 f = f(indf);
 
-figure(5);clf(5)
+figure(4);clf(4)
 subplot(3,1,1)
 l = max(max(max(abs(psd_tf(indf,:,1)))),max(max(abs(psd_tf(indf,:,1)))));
 imagesc((-halfwindow:halfwindow)/Hz,f,psd_tf(:,indf,1)',[-l l]);colorbar
 hold on; plot([0 0],[4 30],'k'); hold off
 xlabel('Time (s)','FontSize',14); ylabel('Frequency (Hz)','FontSize',14);
-title('Power channel 1','FontSize',16)
+title('Power channel 1 : HMM-MAR','FontSize',16)
 subplot(3,1,2)
 imagesc((-halfwindow:halfwindow)/Hz,f,psd_tf(:,indf,2)',[-l l]);colorbar
 hold on; plot([0 0],[4 30],'k'); hold off
 xlabel('Time (s)','FontSize',14); ylabel('Frequency (Hz)','FontSize',14);
-title('Power channel 2','FontSize',16)
+title('Power channel 2 : HMM-MAR','FontSize',16)
 subplot(3,1,3)
 l = max(max(abs(coh_tf(indf,:,1,2))));
 imagesc((-halfwindow:halfwindow)/Hz,f,coh_tf(:,indf,1,2)',[-l l]);colorbar
 hold on; plot([0 0],[4 30],'k'); hold off
 xlabel('Time (s)','FontSize',14); ylabel('Frequency (Hz)','FontSize',14);
-title('Coherence','FontSize',16)
+title('Coherence : HMM-MAR','FontSize',16)
 
 
-%%
-% Probability of transit between states
+%% Probability of transit between states for the 
+% HMM on power envelopes and HMM-MAR
 
-P = hmm_raw.P; 
+P_env = hmm_env.P; 
+% We normalize, such that we focus on the between state transitions
 for k=1:3
-    P(k,k) = 0;
-    P(k,:) = P(k,:) / sum(P(k,:));
+    P_env(k,k) = 0;
+    P_env(k,:) = P_env(k,:) / sum(P_env(k,:));
 end
-P
+disp('HMM-Gaussian Probability of transition from state i to state j')
+P_env
 
-figure(6)
-imagesc(P);colorbar
-title('Transition probability matrix','FontSize',16)
+
+P_raw = hmm_raw.P; 
+% We normalize, such that we focus on the between state transitions
+for k=1:3
+    P_raw(k,k) = 0;
+    P_raw(k,:) = P_raw(k,:) / sum(P_raw(k,:));
+end
+disp('HMM-MAR Probability of transition from state i to state j')
+P_raw
+
+
+
+figure(5)
+
+% plot trans prob matrix for the HMM-Gaussian
+subplot(1,2,1)
+imagesc(P_env);colorbar
+title('Transition probability matrix for the HMM-Gaussian','FontSize',16)
 set(gca,'Xtick',1:3,'ytick',1:3,'FontSize',14)
 xlabel('To','FontSize',16)
 ylabel('From','FontSize',16)
+
+% plot trans prob matrix for the HMM-MAR
+subplot(1,2,2)
+imagesc(P_raw);colorbar
+title('Transition probability matrix for the HMM-MAR','FontSize',16)
+set(gca,'Xtick',1:3,'ytick',1:3,'FontSize',14)
+xlabel('To','FontSize',16)
+ylabel('From','FontSize',16)
+
+
