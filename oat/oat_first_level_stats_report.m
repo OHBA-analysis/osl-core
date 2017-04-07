66be26e1bd594e464e1dad18c953a3b659a34317
Andrew Quinn <andrew.quinn@psych.ox.ac.uk>
2017-04-07 14:19:03 +0100

    Fix first_level report plotting for sensorspace, also trailling whitespace

--- oat/oat_first_level_stats_report.m
+++ oat/oat_first_level_stats_report.m
@@ -7,7 +7,7 @@ function report=oat_first_level_stats_report(oat,first_level_results,report_dir_
 % Can adjust behaviour by changing oat.first_level.report
 % settings, e.g.
 % oat.first_level.report.modality_to_do (only in sensor space)
-% oat.first_level.report.first_level_cons_to_do; % plots only these contrasts, 
+% oat.first_level.report.first_level_cons_to_do; % plots only these contrasts,
 % and uses first one in list to determine max vox, time, freq
 % oat.first_level.report.time_range; % to determine max vox, time, freq
 % oat.first_level.report.freq_range; % to determine max vox, time, freq
@@ -20,7 +20,7 @@ if nargin<3,
     report_dir_in='';
 end;
 
-% contrasts                                  
+% contrasts
 contrast_list=oat.first_level.contrast;
 for c=1:length(contrast_list),
     contrast_list{c}=contrast_list{c}(:);
@@ -35,7 +35,7 @@ end;
 first_level_cons_to_do=oat.first_level.report.first_level_cons_to_do; % plots all of these contrasts
 
 max_cons=6;
-if(length(first_level_cons_to_do)>max_cons)                
+if(length(first_level_cons_to_do)>max_cons)
     disp(['Only plotting reports for the first ' num2str(max_cons) ' contrasts']);
     first_level_cons_to_do=first_level_cons_to_do(1:max_cons);
 end;
@@ -43,11 +43,11 @@ end;
 times=first_level_results.times;
 frequencies=first_level_results.frequencies;
 ntpts = length(times);
-nfreqs = length(frequencies); 
-is_sensor_space=strcmp(first_level_results.recon_method,'none'); % sensor space analysis                    
+nfreqs = length(frequencies);
+is_sensor_space=strcmp(first_level_results.recon_method,'none'); % sensor space analysis
 
 %%%%%%%%%%%%%%%%%%%
-%% set session specific first level diagnostic report up 
+%% set session specific first level diagnostic report up
 if isempty(report_dir_in),
     report_dir=[first_level_results.report.dir '/sess_' first_level_results.session_name];
 else
@@ -59,17 +59,17 @@ if(is_sensor_space)
     report_dir=[report_dir '_' oat.first_level.report.modality_to_do];
     report_name=[report_name ', ' oat.first_level.report.modality_to_do];
 end;
-report=osl_report_setup(report_dir,report_name);   
-   
+report=osl_report_setup(report_dir,report_name);
+
 %%%%%%%%%%%%%%%%%%%
 %% diagnostic plot of design matrix
 
-% diagnostic plot of design matrix      
+% diagnostic plot of design matrix
 report=osl_report_set_figs(report,'design_matrix',sfigure,'Design Matrix');
 title('GLM design matrix');
 
 if(size(first_level_results.cope,2)==1)
-    imagesc(first_level_results.D_sensor_data.time(first_level_results.tf_time_indices_into_D_times),1:size(first_level_results.x,2),first_level_results.x');       
+    imagesc(first_level_results.D_sensor_data.time(first_level_results.tf_time_indices_into_D_times),1:size(first_level_results.x,2),first_level_results.x');
     plot4paper('time (secs)','regressor no.');
 else
     imagesc(first_level_results.x');
@@ -77,17 +77,17 @@ else
 end;
 colorbar;
 report=osl_report_print_figs(report);
-    
-if is_sensor_space,                   
+
+if is_sensor_space,
 
     % find max voxel
-    S2=oat.first_level.report; 
+    S2=oat.first_level.report;
     S2.stats=first_level_results;
     S2.modality=oat.first_level.report.modality_to_do;
     [vox_ind_max time_ind_max freq_ind_max stats max_stat] = oat_find_max_stats( S2 );
 
     if(ntpts>1)
-        
+
         % stats plots at max voxel
         S2=[];
         S2.stats=stats;
@@ -95,12 +95,12 @@ if is_sensor_space,
         S2.first_level_cons_to_do=first_level_cons_to_do; % plots all of these contrasts
         [fig_handle fig_name fig_title] = oat_plot_vox_stats(S2);
         fig_title{1}=[fig_title{1} ' (Max found using c' num2str(first_level_cons_to_do(1)) ')'];
-        
-        report=osl_report_set_figs(report,fig_name,fig_handle,fig_title);              
+
+        report=osl_report_set_figs(report,fig_name,fig_handle,fig_title);
         report=osl_report_print_figs(report);
-        
+
     end;
-         
+
     %%%%%%%%%%%%%
     %% need to call oat_stats_multiplotER to get dat and cfg
     clear cfg dats
@@ -109,89 +109,91 @@ if is_sensor_space,
     S2.oat=oat;
     S2.stats_fname=first_level_results;
     S2.modality=oat.first_level.report.modality_to_do;
-    S2.do_plots=0;
+    S2.do_plots=1;
 
     %%%%%%%%%%%%%
-    %% plot topography at time point with max tstat            
-    for coni=1:length(first_level_cons_to_do),  
-        con=first_level_cons_to_do(coni); 
+    %% plot topography at time point with max tstat
+
+    fig_handle=[];
+    fig_name={};
+    fig_title={};
+    S2.view_cope=1;
+
+    for coni=1:length(first_level_cons_to_do),
+        con=first_level_cons_to_do(coni);
         S2.first_level_contrast=con;
 
-        fig_handle=[];
-        fig_name={};
-        fig_title={};
-        S2.view_cope=1;
-                    
         if(nfreqs>1),
-            
+
             [cfg, dat, fig_handle_tmp]=oat_stats_multiplotTFR(S2);
-                    
+
             cfg.xlim        = [first_level_results.times(time_ind_max) first_level_results.times(time_ind_max)];
             cfg.ylim        = [first_level_results.frequencies(freq_ind_max) first_level_results.frequencies(freq_ind_max)];
             cfg.zlim        = 'maxmin';
             cfg.interactive = 'no';
-            cfg.colorbar    = 'yes';   
-            
-            fig_name{1}=['cope_at_max_multiplot_c' num2str(con)];  
-            fig_title{1}=['COPE for c' num2str(con) ' [' first_level_results.first_level_contrast_name{con} ']']; 
-            fig_title{1}=[fig_title{1} ' (Max found using c' num2str(first_level_cons_to_do(1)) ')'];
-        
+            cfg.colorbar    = 'yes';
+
+            fig_handle(coni) = sfigure;
+            fig_name{coni}=['cope_at_max_multiplot_c' num2str(con)];
+            fig_title{coni}=['COPE for c' num2str(con) ' [' first_level_results.first_level_contrast_name{con} ']'];
+            fig_title{coni}=[fig_title{coni} ' (Max found using c' num2str(first_level_cons_to_do(1)) ')'];
+
             ft_topoplotTFR(cfg,dat);axis tight;
             title([oat.first_level.report.modality_to_do]);
-            
-            report=osl_report_set_figs(report,fig_name,fig_handle,fig_title);                    
-            report=osl_report_print_figs(report);
+
+
         else
             S2.do_plots=1;
-            
-            fig_name{1}=['cope_at_max_multiplot_c' num2str(con)];  
-            fig_title{1}=['COPE for c' num2str(con) ' [' first_level_results.first_level_contrast_name{con} ']']; 
+
+            fig_name{1}=['cope_at_max_multiplot_c' num2str(con)];
+            fig_title{1}=['COPE for c' num2str(con) ' [' first_level_results.first_level_contrast_name{con} ']'];
             fig_title{1}=[fig_title{1} ' (Max found using c' num2str(first_level_cons_to_do(1)) ')'];
-        
+
             [cfg, dat, fig_handle(1)]=oat_stats_multiplotER(S2);
             view([90 90])
-            
-            fig_handle(2)=sfigure; 
-            fig_name{2}=['cope_at_max_topo_c' num2str(con)];  
-            fig_title{2}=['COPE for c' num2str(con) ' [' first_level_results.first_level_contrast_name{con} ']']; 
+
+            fig_handle(2)=sfigure;
+            fig_name{2}=['cope_at_max_topo_c' num2str(con)];
+            fig_title{2}=['COPE for c' num2str(con) ' [' first_level_results.first_level_contrast_name{con} ']'];
             fig_title{2}=[fig_title{2} ' (Max found using c' num2str(first_level_cons_to_do(1)) ')'];
-        
+
             cfg.xlim        = [first_level_results.times(time_ind_max) first_level_results.times(time_ind_max)];
             cfg.ylim        = 'maxmin';
             cfg.zlim        = 'maxmin';
             cfg.interactive = 'no';
             cfg.colorbar    = 'yes';
-            
+
             ft_topoplotTFR(cfg,dat{1});axis tight;
             title([oat.first_level.report.modality_to_do]);
-            
-            report=osl_report_set_figs(report,fig_name,fig_handle,fig_title);                    
+
+            report=osl_report_set_figs(report,fig_name,fig_handle,fig_title);
             report=osl_report_print_figs(report);
         end;
-              
-    end;                           
 
+    end;
+        report=osl_report_set_figs(report,fig_name,fig_handle,fig_title);
+        report=osl_report_print_figs(report);
 else
-    
+
     % source space
-    
+
     % find max voxel
-    S2=oat.first_level.report;    
-    S2.stats=first_level_results;               
+    S2=oat.first_level.report;
+    S2.stats=first_level_results;
     [vox_ind_max time_ind_max freq_ind_max stats] = oat_find_max_stats( S2 );
 
-    if(ntpts>1)       
+    if(ntpts>1)
         % stats plots at max voxel
         S2=[];
         S2.stats=stats;
         S2.vox_coord=stats.mni_coords(vox_ind_max,:); % can change to this setting to plot the time courses at the voxel with the max t-stat
         S2.first_level_cons_to_do=first_level_cons_to_do; % plots all of these contrasts
-        [fig_handle fig_name fig_title] = oat_plot_vox_stats(S2);   
-        fig_title{1}=[fig_title{1} ' (Max found using c' num2str(first_level_cons_to_do(1)) ')'];        
-        report=osl_report_set_figs(report,fig_name,fig_handle,fig_title);        
+        [fig_handle fig_name fig_title] = oat_plot_vox_stats(S2);
+        fig_title{1}=[fig_title{1} ' (Max found using c' num2str(first_level_cons_to_do(1)) ')'];
+        report=osl_report_set_figs(report,fig_name,fig_handle,fig_title);
         report=osl_report_print_figs(report);
     end;
-    
+
     if ~isfield(oat.first_level, 'mni_coords'),
 
         %%%%%%%%%%%%%%%%%%%
@@ -210,39 +212,39 @@ else
         [statsdir,times,count]=oat_save_nii_stats(S2);
 
         %%%%%%%%%%%%%
-        %% plot cope orthoviews at time point (and freq) with max tstat 
+        %% plot cope orthoviews at time point (and freq) with max tstat
         fig_handle=[];
         fig_name={};
-        fig_title={}; 
-        S2=[];                
-        
+        fig_title={};
+        S2=[];
+
         S2.percrange=[96 99.9];
         S2.mni_coord=first_level_results.mni_coords(vox_ind_max,:);
         S2.title='';
 
-        for coni=1:length(first_level_cons_to_do),  
-            con=first_level_cons_to_do(coni); 
+        for coni=1:length(first_level_cons_to_do),
+            con=first_level_cons_to_do(coni);
 
-            fig_handle(coni)=sfigure; 
-            fig_name{coni}=['cope_at_maxt_smap_c' num2str(con)];  
-            fig_title{coni}=['COPE for c' num2str(con) ' [' first_level_results.first_level_contrast_name{con} '] at t=' num2str(first_level_results.times(time_ind_max)) 'secs' ', f=' mat2str(first_level_results.frequency_ranges(freq_ind_max,:),3) 'Hz'];                 
+            fig_handle(coni)=sfigure;
+            fig_name{coni}=['cope_at_maxt_smap_c' num2str(con)];
+            fig_title{coni}=['COPE for c' num2str(con) ' [' first_level_results.first_level_contrast_name{con} '] at t=' num2str(first_level_results.times(time_ind_max)) 'secs' ', f=' mat2str(first_level_results.frequency_ranges(freq_ind_max,:),3) 'Hz'];
             fig_title{coni}=[fig_title{coni} ' (Max found using c' num2str(first_level_cons_to_do(1)) ')'];
             set(fig_handle(coni),'Position',[1 1 1300 450]);
-            S2.fname=[statsdir '/cope' num2str(con) '_' num2str(resamp_gridstep) 'mm'];                              
-            ortho_overlay_act( S2 );                     
+            S2.fname=[statsdir '/cope' num2str(con) '_' num2str(resamp_gridstep) 'mm'];
+            ortho_overlay_act( S2 );
 
         end;
-    
+
     end;
 
-    report=osl_report_set_figs(report,fig_name,fig_handle,fig_title);        
+    report=osl_report_set_figs(report,fig_name,fig_handle,fig_title);
     report=osl_report_print_figs(report);
 
-end;    
+end;
 
 %%%%%%%%%%%%%%%%%%%
 %% generate source recon web report
-report=osl_report_write(report);        
+report=osl_report_write(report);
 disp(['View first level report at:']);
 disp([report.html_fname]);
 
