18928f7ff1299ee1c39ab59657104c8769559d59
Robert Becker <robert.becker@psych.ox.ac.uk>
2017-04-18 20:23:06 +0100

    Improved checks for path correctness

--- util/osl_relink_Dinv.m
+++ util/osl_relink_Dinv.m
@@ -17,27 +17,50 @@ Dout=Din;
 for invs=1:length(Din.inv)
     begstr=strfind(Din.inv{invs}.mesh.sMRI,oldpath);
     endstr=length(oldpath)+begstr;
-    Dout.inv{invs}.mesh.sMRI=prefix(Din.inv{invs}.mesh.sMRI(endstr:end),newpath);
+    if ~(isempty(begstr))
+        Dout.inv{invs}.mesh.sMRI=prefix(Din.inv{invs}.mesh.sMRI(endstr:end),newpath);
+    else
+        warning('Path not found')
+    end
     
     begstr=strfind(Din.inv{invs}.mesh.def,oldpath);
     endstr=length(oldpath)+begstr;
-    Dout.inv{invs}.mesh.def=prefix(Din.inv{invs}.mesh.def(endstr:end),newpath);
-
+    if ~(isempty(begstr))
+        Dout.inv{invs}.mesh.def=prefix(Din.inv{invs}.mesh.def(endstr:end),newpath);
+    else
+        warning('Path not found')
+    end
+    
     begstr=strfind(Din.inv{invs}.mesh.tess_ctx,oldpath);
     endstr=length(oldpath)+begstr;
-    Dout.inv{invs}.mesh.tess_ctx=prefix(Din.inv{invs}.mesh.tess_ctx(endstr:end),newpath);
-
+    if ~(isempty(begstr))
+        Dout.inv{invs}.mesh.tess_ctx=prefix(Din.inv{invs}.mesh.tess_ctx(endstr:end),newpath);
+    else
+        warning('Path not found')
+    end
+    
     begstr=strfind(Din.inv{invs}.mesh.tess_scalp,oldpath);
     endstr=length(oldpath)+begstr;
-    Dout.inv{invs}.mesh.tess_scalp=prefix(Din.inv{invs}.mesh.tess_scalp(endstr:end),newpath);
-
+    if ~(isempty(begstr))
+        Dout.inv{invs}.mesh.tess_scalp=prefix(Din.inv{invs}.mesh.tess_scalp(endstr:end),newpath);
+    else
+        warning('Path not found')
+    end
+    
     begstr=strfind(Din.inv{invs}.mesh.tess_oskull,oldpath);
     endstr=length(oldpath)+begstr;
-    Dout.inv{invs}.mesh.tess_oskull=prefix(Din.inv{invs}.mesh.tess_oskull(endstr:end),newpath);
-
+    if ~(isempty(begstr))
+        Dout.inv{invs}.mesh.tess_oskull=prefix(Din.inv{invs}.mesh.tess_oskull(endstr:end),newpath);
+    else
+        warning('Path not found')
+    end
+    
     begstr=strfind(Din.inv{invs}.mesh.tess_iskull,oldpath);
     endstr=length(oldpath)+begstr;
-    Dout.inv{invs}.mesh.tess_iskull=prefix(Din.inv{invs}.mesh.tess_iskull(endstr:end),newpath);
+    if ~(isempty(begstr))
+        Dout.inv{invs}.mesh.tess_iskull=prefix(Din.inv{invs}.mesh.tess_iskull(endstr:end),newpath);
+    else
+        warning('Path not found')
+    end
 end
-            
-            
+
