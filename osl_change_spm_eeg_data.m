9dcf96641510f6538a8b581c2a950addf398762b
Andrew Quinn <andrew.quinn@psych.ox.ac.uk>
2017-04-19 15:21:42 +0100

    Ensure keep data if not changing montage and use consistent object in later script

--- osl_change_spm_eeg_data.m
+++ osl_change_spm_eeg_data.m
@@ -27,6 +27,8 @@ try, chantype=Sc.chantype; catch, chantype='LFP';  end;
 % remove montages
 if Sc.remove_montages
     D=montage(Sc.D,'remove',1:montage(Sc.D,'getnumber'));
+else
+    D=Sc.D;
 end
 
 if(size(Sc.newdata,4)==1)
@@ -75,14 +77,14 @@ end
 if size(Sc.D,1) == size(Sc.newdata,1)
     % We aren't changing the number of channels, keep all information
     if(length(Sc.D.badchannels)>0)
-        D2 = badchannels(D2, Sc.D.badchannels, ones(length(Sc.D.badchannels),1));
+        D2 = badchannels(D2, D.badchannels, ones(length(D.badchannels),1));
     end
-    D2=D2.chantype(chanind, Sc.D.chantype);
-    D2=D2.chanlabels(chanind, Sc.D.chanlabels);
+    D2=D2.chantype(chanind, D.chantype);
+    D2=D2.chanlabels(chanind, D.chanlabels);
 elseif ~isempty(modalities)
     % We are using a subset of modalities, keep only their information
-    D2=D2.chantype(chanind, Sc.D.chantype(chanind));
-    D2=D2.chanlabels(chanind, Sc.D.chanlabels(chanind));
+    D2=D2.chantype(chanind, D.chantype(chanind));
+    D2=D2.chanlabels(chanind, D.chanlabels(chanind));
 else
     % We are completely changing the channels, change type to LFP
     D2=D2.chantype(chanind, chantype);
