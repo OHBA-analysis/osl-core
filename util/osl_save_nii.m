c89a9b4bee405568d3d0032b04672ff1eff17792
Romesh Abeysuriya <romesh.abeysuriya@psych.ox.ac.uk>
2017-07-01 19:35:49 -0400

    Improve comments, fix order of setting qform/xform

--- util/osl_save_nii.m
+++ util/osl_save_nii.m
@@ -1,10 +1,13 @@
-function save_nii(vol,res,xform,fname)
+function osl_save_nii(vol,res,xform,fname)
 	% Save a nii file together with a given xform matrix
 	%
 	% INPUTS
 	% vol - volume matrix to save to nii file
+	% res - Spatial resolution e.g. '2' if same in all dimensions, or '[2 2 2]' to specify independently
 	% fname - File name of nii file to save
-	% xform - 4x4 matrix
+	% xform - 4x4 matrix. 
+	%
+	% Could use osl_load_nii to get res and xform from a standard mask
 	%
 	% Romesh Abeysuriya 2017
 	
@@ -13,8 +16,8 @@ function save_nii(vol,res,xform,fname)
     else
         save_avw(vol,fname,'d',[res 1]); %MWW
     end
-    
+
+    runcmd(['fslorient -setqform ' num2str(reshape(xform',1,16)) ' ' fname])
+    runcmd(['fslorient -setsform ' num2str(reshape(xform',1,16)) ' ' fname])
 	runcmd(['fslorient -setsformcode 0 ' fname])
 	runcmd(['fslorient -setqformcode 2 ' fname])
-	runcmd(['fslorient -setqform ' num2str(reshape(xform',1,16)) ' ' fname])
-	runcmd(['fslorient -setsform ' num2str(reshape(xform',1,16)) ' ' fname])
