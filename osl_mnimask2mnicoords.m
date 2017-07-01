443c21022b2c7d15418c6afa1db10229e56cda0e
Romesh Abeysuriya <romesh.abeysuriya@psych.ox.ac.uk>
2017-07-01 19:41:04 -0400

    Remove redundant variable

--- osl_mnimask2mnicoords.m
+++ osl_mnimask2mnicoords.m
@@ -1,4 +1,4 @@
-function [ mni_coords xform ] = osl_mnimask2mnicoords(fname)
+function [ mni_coords, xform ] = osl_mnimask2mnicoords(fname)
 	% [ mni_coords xform ] = osl_mnimask2mnicoords(mask)
 	%
 	% converts an MNI standard brain mask into a list of mni_coords.
@@ -9,7 +9,7 @@ function [ mni_coords xform ] = osl_mnimask2mnicoords(fname)
 	% Romesh Abeysuriya 2017
 	% MW (pre-2014)
 	
-	[vol,res,xform] = osl_load_nii(fname); 
+	[vol,~,xform] = osl_load_nii(fname); 
 	[x,y,z] = ind2sub(size(vol),find(vol));	% Convert nonzero entries in mask to array indices
 	mni_coords = xform*[(x-1)';(y-1)';(z-1)';ones(size(x'))]; % Use xform to convert indices to the MNI coordinates
 	mni_coords = mni_coords(1:3,:)';
