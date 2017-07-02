8462b07548b21bd3aa65aab5fa8f5490d91a7c26
Romesh Abeysuriya <romesh.abeysuriya@psych.ox.ac.uk>
2017-07-02 17:12:12 +0100

    Comment out lines that don't currently work

--- examples/oat/osl_example_beamformer_oat.m
+++ examples/oat/osl_example_beamformer_oat.m
@@ -219,8 +219,8 @@ S2.resamp_gridstep=oat.source_recon.gridstep;
 mni_brain=[osldir '/std_masks/MNI152_T1_' num2str(S2.resamp_gridstep) 'mm_brain.nii.gz'];
 
 % Inspect the results of an OAT contrast in FSLVIEW
-contrast=;
-runcmd(['fslview ' mni_brain ' ' [statsdir '/tstat' num2str(contrast) '_' num2str(S2.resamp_gridstep) 'mm'] ' &']);
+% contrast=;
+% runcmd(['fslview ' mni_brain ' ' [statsdir '/tstat' num2str(contrast) '_' num2str(S2.resamp_gridstep) 'mm'] ' &']);
 
 %% VIEW RESULTS IN FLSVIEW
 %
