eb5c2af2a5b1692576f15a5f2478fcef6229430b
Romesh Abeysuriya <romesh.abeysuriya@psych.ox.ac.uk>
2017-08-01 16:54:24 +0100

    Display head upright

--- rhino/rhino_display.m
+++ rhino/rhino_display.m
@@ -26,13 +26,10 @@ end
 if any(strcmp(get(get(hf,'children'),'type'),'axes'))
     [az,el] = view;
 else
-    az = 90; el = 0;
+    az = 125; el = 15;
 end
 
 ha = axes('parent',hf); hold on
-
-
-set(hf,'WindowButtonMotionFcn',@movelight,'Interruptible','off','busyaction','cancel');
 
 rotate3d(hf,'ON');
 
@@ -50,8 +47,7 @@ headshape_polhemus = D.inv{1}.datareg(1).fid_eeg.pnt;
 fid_polhemus = D.inv{1}.datareg(1).fid_eeg.fid.pnt;
 fid_mri = D.inv{1}.datareg(1).fid_mri.fid.pnt;
 
-patch(mesh_rhino,'FaceColor',[238,206,179]./255,'EdgeColor','none','FaceAlpha',0.7,'Parent',ha);
-
+patch(mesh_rhino,'FaceColor',[238,206,179]./255,'EdgeColor','none','FaceAlpha',0.6,'Parent',ha);
 patch(struct('faces',mesh.tess_ctx.face,'vertices',mesh.tess_ctx.vert),'FaceColor',[1 0.7 0.7],'EdgeColor','none','Parent',ha);
 %patch(struct('faces',mesh.tess_scalp.face,'vertices',mesh.tess_scalp.vert),'EdgeColor','g','FaceColor','none','Parent',ha);
 
@@ -61,6 +57,8 @@ axis(ha,'image','off')
 material shiny
 lighting gouraud
 hl = camlight('headlight');
+set(hf,'WindowButtonMotionFcn',@(~,~) camlight(hl,'headlight'),'Interruptible','off','busyaction','cancel');
+
 
 % Headshape locations
 %--------------------------------------------------------------------------
@@ -89,9 +87,111 @@ set(h_fid,'MarkerFaceColor','c','MarkerSize',12,'MarkerEdgeColor','k');
 h_fidmr = plot3(fid_mri(:,1),fid_mri(:,2),fid_mri(:,3),'dm');
 set(h_fidmr,'MarkerFaceColor','m','MarkerSize',12,'MarkerEdgeColor','k');
 
+material dull % Improve visibility of the brain by reducing reflection glare
 
-    function movelight(varargin)
-        hl = camlight(hl,'headlight');
-    end
+% Make the head upright
+%Currently DOES NOT rotate the scatter objects
+c = get(ha,'Children');
+local_rotate(c,[0 1 0],-35)
 
 end
+
+function local_rotate(h,azel,alpha,origin)
+%% Patched version of 'rotate' from Matlab
+
+% Determine the default origin (center of plot box).
+if nargin < 4
+  if ~ishghandle(h)
+    error(message('MATLAB:rotate:InvalidHandle'));
+  end
+  ax = ancestor(h(1),'axes');
+  if isempty(ax) || ax==0,
+    error(message('MATLAB:rotate:InvalidHandle'));
+  end
+  matlab.ui.internal.UnsupportedInUifigure(ancestor(ax,'figure'));
+  origin = sum([get(ax,'xlim')' get(ax,'ylim')' get(ax,'zlim')'])/2;
+end
+
+% find unit vector for axis of rotation
+if numel(azel) == 2 % theta, phi
+    theta = pi*azel(1)/180;
+    phi = pi*azel(2)/180;
+    u = [cos(phi)*cos(theta); cos(phi)*sin(theta); sin(phi)];
+elseif numel(azel) == 3 % direction vector
+    u = azel(:)/norm(azel);
+end
+
+alph = alpha*pi/180;
+cosa = cos(alph);
+sina = sin(alph);
+vera = 1 - cosa;
+x = u(1);
+y = u(2);
+z = u(3);
+rot = [cosa+x^2*vera x*y*vera-z*sina x*z*vera+y*sina; ...
+       x*y*vera+z*sina cosa+y^2*vera y*z*vera-x*sina; ...
+       x*z*vera-y*sina y*z*vera+x*sina cosa+z^2*vera]';
+
+for i=1:numel(h),
+  t = get(h(i),'type');
+  skip = 0;
+  t
+  if strcmp(t,'surface') || strcmp(t,'line') || strcmp(t,'patch') || strcmp(t,'scatter')
+    
+    % If patch, rotate vertices  
+    if strcmp(t,'patch')
+       verts = get(h(i),'Vertices');
+       x = verts(:,1); y = verts(:,2); 
+       if size(verts,2)>2
+          z = verts(:,3);
+       else
+          z = [];
+       end
+       
+    % If surface or line, rotate {x,y,z}data   
+    else
+       x = get(h(i),'xdata');
+       y = get(h(i),'ydata');
+       z = get(h(i),'zdata');
+    end
+    
+    if isempty(z)
+       z = -origin(3)*ones(size(y));
+    end
+    [m,n] = size(z);
+    if numel(x) < m*n
+      [x,y] = meshgrid(x,y);
+    end
+  elseif strcmp(t,'text')
+    p = get(h(i),'position');
+    x = p(1); y = p(2); z = p(3);
+  elseif strcmp(t,'image')
+    x = get(h(i),'xdata');
+    y = get(h(i),'ydata');
+    z = zeros(size(x));
+  else
+    skip = 1;
+  end
+  
+  if ~skip,
+    [m,n] = size(x);
+    newxyz = [x(:)-origin(1), y(:)-origin(2), z(:)-origin(3)];
+    newxyz = newxyz*rot;
+    newx = origin(1) + reshape(newxyz(:,1),m,n);
+    newy = origin(2) + reshape(newxyz(:,2),m,n);
+    newz = origin(3) + reshape(newxyz(:,3),m,n);
+
+    if strcmp(t,'surface') || strcmp(t,'line') || strcmp(t,'scatter')
+      set(h(i),'xdata',newx,'ydata',newy,'zdata',newz);
+    elseif strcmp(t,'patch')
+      set(h(i),'Vertices',[newx,newy,newz]); 
+    elseif strcmp(t,'text')
+      set(h(i),'position',[newx newy newz])
+    elseif strcmp(t,'image')
+      set(h(i),'xdata',newx,'ydata',newy)
+    end
+  end
+end
+end
+
+
