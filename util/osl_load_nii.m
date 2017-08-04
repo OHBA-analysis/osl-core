c9dbb0bca42a2f0821c9e2545d05f78dbca49e98
Romesh Abeysuriya <romesh.abeysuriya@psych.ox.ac.uk>
2017-08-04 10:46:39 +0100

    Reading via Matlab tools as well

--- util/osl_load_nii.m
+++ util/osl_load_nii.m
@@ -1,9 +1,25 @@
-function [vol,res,xform] = load_nii(fname)
+function [vol,res,xform] = osl_load_nii(fname)
 	% Load a nii volume together with the spatial resolution and xform matrix
 	%
 	% Romesh Abeysuriya 2017
 	assert(ischar(fname),'Input file name must be a string')
 	assert(logical(exist(fname,'file')),sprintf('Requested file "%s" could not be found',fname))
-	[vol,~,scales] = read_avw(fname);
-	res = scales(1:3).';
-	xform = reshape(str2num(runcmd('fslorient -getsform %s',fname)),4,4)';
+
+	nii = load_untouch_nii(fname);
+	vol = nii.img;
+	res = nii.hdr.dime.pixdim(2:4);
+	xform = eye(4);
+
+	if nii.hdr.hist.sform_code == 0 &&  nii.hdr.hist.qform_code == 0
+		fprintf(2,'Warning - *NO* qform/sform header code is provided! Your NIFTI file is *not* usable without this information!\n')
+	elseif nii.hdr.hist.sform_code == 0
+		fprintf(2,'Warning - osl_load_nii() currently only supports sform (not qform)!\n')
+	end
+
+	xform(1,:) = nii.hdr.hist.srow_x;
+	xform(2,:) = nii.hdr.hist.srow_y;
+	xform(3,:) = nii.hdr.hist.srow_z;
+
+
+
+
