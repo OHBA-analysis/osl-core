1acf9567515f24f3a26b1f0219a2084237f3221f
Romesh Abeysuriya <romesh.abeysuriya@psych.ox.ac.uk>
2017-08-06 14:24:44 +0100

    Use MATLAB tools to read spatial resolution

--- util/get_nii_spatial_res.m
+++ util/get_nii_spatial_res.m
@@ -5,24 +5,12 @@ function [ mni_res ] = get_nii_spatial_res( mask_fname )
     % RA - runcmd() will gracefully display a 'command not found' error if FSLDIR is not 
     % correctly set, and this is also checked by osl2_startup.m, so no need to check here
     % as well
-
-    res=[];
-
+    
     assert(exist(mask_fname,'file')~=0,'File %s not found',mask_fname);
+    nii = load_untouch_nii(mask_fname);
+    mni_res = nii.hdr.dime.pixdim(2:4);
 
-    res = runcmd(['fslval ' mask_fname ' pixdim1']);
-    mni_res(1)=str2double(res);
-    assert(isfinite(mni_res(1)));
 
-    res=runcmd(['fslval ' mask_fname ' pixdim2']);
-    mni_res(2)=str2num(res);
-
-    res=runcmd(['fslval ' mask_fname ' pixdim3']);
-    mni_res(3)=str2num(res);
-
-    if(mni_res(1)~=mni_res(2) | mni_res(2)~=mni_res(3))
-        error('all pixel dimensions should be the same for the input nii');
-    end 
 
 
 
