47d3b15c98f7e2aa6c7ce2e17e6c8621d357646c
Romesh Abeysuriya <romesh.abeysuriya@psych.ox.ac.uk>
2017-08-15 16:17:04 +0100

    Remove unused runcmd wrappers

--- util/call_fsl.m
+++
@@ -1,8 +0,0 @@
-function [status,output] = call_fsl(cmd)
-	% OSL handles FSL setup, so this function is used to override the
-	% version of call_fsl.m that comes with FSL to avoid issues related
-	% to environment variables being cleared in the Matlab session
-	%
-	% Note how the order of the outputs is swapped compared to runcmd() to maintain compatibility
-	% with other FSL matlab tools (like nii.load)
-	[output,status] = runcmd(cmd);
