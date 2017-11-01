63b1e3625c7e7e527232f7fddf30f2297780c417
Romesh Abeysuriya <romesh.abeysuriya@psych.ox.ac.uk>
2017-11-01 11:53:04 +0000

    Restore old folder regardless of how function terminated

--- osl_inverse_model.m
+++ osl_inverse_model.m
@@ -266,13 +266,10 @@ matlabbatch{5}.spm.tools.beamforming.output.plugin.montage_osl.normalise        
 matlabbatch{6}.spm.tools.beamforming.write.BF(1)                                = cfg_dep('Output: BF.mat file', substruct('.','val', '{}',{5}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','BF'));
 matlabbatch{6}.spm.tools.beamforming.write.plugin.spmeeg_osl.prefix             = S.prefix;
 
-
+obj = onCleanup(@() cd(old_dir)); % Restore the working directory
 spm_jobman('run',matlabbatch)
-
 D = spm_eeg_load(D.fullfile); % Load the file back from disk with the new online montages
-cd(old_dir); % Restore the working directory
 
-end
 
 
 
