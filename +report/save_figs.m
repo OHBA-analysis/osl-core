c101e991335d332cba34a7351fda62b36862f15b
Romesh Abeysuriya <romesh.abeysuriya@psych.ox.ac.uk>
2017-11-09 08:10:20 +0000

    Added ability to add prefix to figure for saving multiple outputs in one directory

--- +report/save_figs.m
+++ +report/save_figs.m
@@ -1,9 +1,15 @@
-function save_figs(h,dirname)
+function save_figs(h,dirname,prefix)
 	% Take in an array of figure handles
 	% Save them to directory 'dirname'
 	% Create directory if required
 	% Automatically infer filename if required
 
+	if nargin < 3 || isempty(prefix) 
+		prefix = '';
+	else
+		prefix = [prefix '_'];
+	end
+	
 	if ~isdir(dirname)
 		mkdir(dirname);
 	end
@@ -15,5 +21,5 @@ function save_figs(h,dirname)
 			fname = ['untitled_' tmp(1:10)];
 		end
 		
-		pfig(h(j),fullfile(dirname,[fname '.png']));
+		pfig(h(j),fullfile(dirname,[prefix fname '.png']));
 	end
