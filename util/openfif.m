3f770f0c2486fa7363e3c819b3289695874a4f39
Romesh Abeysuriya <romeshga@hbaws34.ohba.ox.ac.uk>
2017-11-28 09:16:58 +0000

    Added fif file handler to prevent accidentally opening fif files with Matlab editor

---
+++ util/openfif.m
@@ -0,0 +1,5 @@
+function openfif(filename)
+	% Open nii files in Matlab using osleyes when double clicking
+    [~,fname] = fileparts(filename);
+	fprintf(2,'%s.fif is a binary file, cannot open by double clicking\n',fname);
+    
