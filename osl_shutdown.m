c0216d5c073825049f12f24973cbd61c22cc88f8
Jonathan H <jonathan.hadida@ohba.ox.ac.uk>
2019-10-22 16:38:24 +0100

    Improve doc and fix subheadings

--- osl_shutdown.m
+++ osl_shutdown.m
@@ -1,17 +1,17 @@
-function osl_shutdown(force)
-% Remove OSL from path
+function osl_shutdown( clean_backup_path )
+% 
+% osl_shutdown( clean_backup_path=false )
+% 
+% Restore Matlab path to its original value (before osl_startup was called).
 %
-% Note that this will not remove FSL or workbench from the system path
-% so commands like "system('fslval')" should still run
-% In that respect, there will still be some traces of OSL. It's a bit more 
-% complicated to remove these because they may not have been added by
-% OSL, so best not to worry about them for now. The FSL Matlab helpers
-% may also be left intact, if they were present before OSL was started
+% The folder osl-core itself is often manually added to the path prior to 
+% calling osl_startup (in order to make osl_startup callable). Hence, the 
+% restored path will often still contain osl-core. Set clean_backup_path to
+% true in order to remove OSL folders from the backed up path as well.
 %
-% Romesh Abeysuriya 2017
-% JH 2018
+% RA 2017, JH 2019
 
-    if nargin < 1, force=false; end
+    if nargin < 1, clean_backup_path=false; end
 
     % OSL is initialized if OSLDIR is set. If OSLDIR is not set, then do nothing
     if osl_isactive()
@@ -26,7 +26,7 @@ function osl_shutdown(force)
 
     % It's concievable that the backup path still contains OSL directories; for example
     % it is common to add the osl-core folder manually in order to call osl_startup.
-    if force
+    if clean_backup_path
         p = strsplit(path,pathsep);
         p = p(cellfun( @(x) osl_util.contains(x,osl_root), p ));
         cellfun( @(x) rmpath(x), p );
