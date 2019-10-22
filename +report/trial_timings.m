33895190fe3817378075e4a27452a61e878f1009
Mark Woolrich <woolrich@fmrib.ox.ac.uk>
2019-10-22 16:16:51 +0100

    Now plots bad trials in black

--- +report/trial_timings.m
+++ +report/trial_timings.m
@@ -24,15 +24,24 @@ if nargin==1 || isempty(epochinfo)
         warning('Only works with single input, event_timings(D), if D is epoched data');
         return
     end
-
-    if ~isfield(D,'epochinfo')
-        warning('Inputted epoched SPM MEEG object needs to have an epochinfo field (normally this is set by osl_epoch)');
-        return
-    end
-    D_continuous=D.epochinfo.D;
     
-    epochinfo=D.epochinfo;
-    condlist=D.condlist;
+    try
+        D_continuous=D.epochinfo.D;
+        epochinfo=D.epochinfo;
+        condlist=D.condlist;
+    catch
+        disp('No D.epochinfo');
+        D_continuous=[];
+        D_continuous.fsample = D.fsample;
+        
+        last_trial_start = round(max(D.trialonset)*D.fsample);
+        last_trial_end = last_trial_start + length(D.time) - 1;
+        
+        tres=D.fsample;
+        D_continuous.time = tres:tres:tres*last_trial_end; 
+        
+        condlist=D.condlist;
+    end
     
     D_epoched=D;
     
@@ -55,13 +64,15 @@ else
 end
 clear D;
 
-% epochinfo.trl is [ begin_sample, end_sample, offset ] (in ms) of each 
-% trial (e.g. computed by spm_eeg_definetrial)
-
 % calculate vector of when each condition has a trial happening
 condition_on=zeros(length(condlist),length(D_continuous.time));
 condition_start=zeros(length(condlist),length(D_continuous.time));
 condition_end=zeros(length(condlist),length(D_continuous.time));
+
+condition_on_bad=zeros(length(condlist),length(D_continuous.time));
+condition_start_bad=zeros(length(condlist),length(D_continuous.time));
+condition_end_bad=zeros(length(condlist),length(D_continuous.time));
+
 
 yrange=[0, 0.5];
 
@@ -69,6 +80,10 @@ df=fliplr(linspace(yrange(1),yrange(2),length(condlist)+3));
 for cc=1:length(condlist)            
             
     if isempty(D_epoched)
+        
+        % epochinfo.trl is [ begin_sample, end_sample, offset ] (in ms) of each 
+        % trial (e.g. computed by spm_eeg_definetrial)
+
         for ee=1:size(epochinfo.trl,1)
             if epochinfo.trl(ee,1)>=1 && epochinfo.trl(ee,2)<=length(D_continuous.time)
                 if strcmp(epochinfo.conditionlabels{ee},condlist{cc})
@@ -79,18 +94,24 @@ for cc=1:length(condlist)
             end
         end
     else
-        % note that the trials in epochinfo can be different to those in
-        % D_epoched, due to trials that run off the start or the end of the
-        % continuous data
-        
+
         for ee=1:D_epoched.ntrials
             
             if strcmp(D_epoched.conditions{ee},condlist{cc})
                 start=round(D_epoched.trialonset(ee)*D_epoched.fsample);
 
+                
                 condition_on(cc,start : start + length(D_epoched.time) - 1)=df(cc+1);
                 condition_start(cc,start)=df(cc+1);
                 condition_end(cc,start + length(D_epoched.time) - 1)=df(cc+1);
+                
+                % bad trials 
+                if D_epoched.badtrials(ee)        
+                    condition_on_bad(cc,start : start + length(D_epoched.time) - 1)=df(cc+1);
+                    condition_start_bad(cc,start)=df(cc+1);
+                    condition_end_bad(cc,start + length(D_epoched.time) - 1)=df(cc+1);
+                end
+
             end
             
         end
@@ -105,26 +126,40 @@ hfig = figure('name',[plot_name_prefix 'Event timings'],'tag',[plot_name_prefix 
 hold on;
 pp=[];
 
+% plot trials
 for cc=1:length(condlist)
     tmp=double(condition_on(cc,:));
     tmp(tmp==0)=nan;
     pp(cc)=plot(D_continuous.time, tmp, get_cols(cc), 'LineWidth',8);
+    tmp=double(condition_on_bad(cc,:));
+    tmp(tmp==0)=nan;
+    plot(D_continuous.time, tmp, 'k', 'LineWidth',8);
     
     tmp=double(condition_start(cc,:));
     tmp(tmp==0)=nan;
     plot(D_continuous.time, tmp, [get_cols(cc) 'o'], 'MarkerSize',12, 'LineWidth',2);
+    tmp=double(condition_start_bad(cc,:));
+    tmp(tmp==0)=nan;
+    plot(D_continuous.time, tmp, 'ko', 'MarkerSize',12, 'LineWidth',2);
     
     tmp=double(condition_end(cc,:));
     tmp(tmp==0)=nan;
     plot(D_continuous.time, tmp, [get_cols(cc) 'x'], 'MarkerSize',12, 'LineWidth',2);
+    tmp=double(condition_end_bad(cc,:));
+    tmp(tmp==0)=nan;
+    plot(D_continuous.time, tmp, 'kx', 'MarkerSize',12, 'LineWidth',2);
+
 end
 
-tmp=double(~good_samples(D_continuous,D_continuous.indchantype('MEEG','GOOD')))*df(length(condlist)+2);
-tmp(tmp==0)=nan;
-
-pp(length(condlist)+1)=plot(D_continuous.time, tmp, 'k', 'LineWidth',8);
-
-legend(pp,[condlist, {'Bad samples'}]);
+% plot bad samples from continuous data, if available
+try
+    tmp=double(~good_samples(D_continuous,D_continuous.indchantype('MEEG','GOOD')))*df(length(condlist)+2);
+    tmp(tmp==0)=nan;
+    pp(length(condlist)+1)=plot(D_continuous.time, tmp, 'k', 'LineWidth',8);
+    legend(pp,[condlist, {'Bad samples'}]);
+catch
+    legend(pp,condlist);
+end
 
 set(gca,'YTick',[],'YTickLabel',[]);
 title('Timings of trials and bad samples');
