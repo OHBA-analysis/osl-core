4a33b4c049c443e001aad30342cc47eedae2a78c
Andrew Quinn <andrew.quinn@psych.ox.ac.uk>
2020-04-30 16:02:26 +0100

    Minor formatting update to osl_movcorr.m

--- osl_movcorr.m
+++ osl_movcorr.m
@@ -12,7 +12,7 @@ if ~exist('overlap','var') || isempty(overlap)
   overlap = 0.75;
 end
 
-if ~exist('resamp','var') || isempty(resamp) 
+if ~exist('resamp','var') || isempty(resamp)
   resamp = 0;
 end
 
@@ -21,7 +21,7 @@ if resamp == 1;
 elseif resamp == 0;
   resamp = [];
 end
-  
+
 % Chunk data into overlapping sections
 s1w = buffer(s1,winsize,round(winsize*overlap),'nodelay');
 s2w = buffer(s2,winsize,round(winsize*overlap),'nodelay');
@@ -38,7 +38,7 @@ for i = 1:size(s1w,2)
   % compute the correlation excluding these values.
   if sum(notNaN) < 0.75*size(s1w,1)
     notNaN(:) = 1; % this ensures NaN values are passed to prcorr which will return NaN.
-  end  
+  end
   try
       c(i) = prcorr2(s1w(notNaN,i),s2w(notNaN,i));
   catch
@@ -48,7 +48,7 @@ for i = 1:size(s1w,2)
 end
 
 if ~isempty(resamp)
-  
+
   % Get start and end limits, accounting for zero padding at end of signal
   n_pad = find(cumsum(s1w(end:-1:1,end))==0, 1, 'last' );
   if isempty(n_pad)
@@ -56,9 +56,9 @@ if ~isempty(resamp)
   end
   t_start = round(winsize/2);
   t_end = length(s1) - round(winsize/2) + n_pad;
-  
-  
-  
+
+
+
   % Resample to same size as original data
   ti = t_start:1:t_end;
   t = linspace(t_start,t_end,length(c));
@@ -66,12 +66,11 @@ if ~isempty(resamp)
   warning('off', 'MATLAB:interp1:NaNinY');
   ci(ti) = interp1(t,c,ti,resamp);
   warning('on', 'MATLAB:interp1:NaNinY');
-  
+
   % Remove excess at end due to padding
-  ci(end-round(winsize/2)+1:end) = NaN; 
-  
+  ci(end-round(winsize/2)+1:end) = NaN;
+
   c = ci;
 end
 
 end
- 
