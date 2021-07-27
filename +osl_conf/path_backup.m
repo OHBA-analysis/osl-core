e5aa7fca30d2bed414ab78ab1a5af8d41505db63
Andrew Quinn <andrew.quinn@psych.ox.ac.uk>
2021-07-27 17:37:14 +0100

    More useful logger message in path backup at startup

--- +osl_conf/path_backup.m
+++ +osl_conf/path_backup.m
@@ -2,18 +2,19 @@ function p = path_backup()
 %
 % Used at startup to backup the Matlab path.
 % When calling osl_shutdown, the backup is used to restore the original path.
-% 
+%
 % See also: osl_startup, osl_util.path_restore
 %
 % JH
 
-    p = path(); 
+    p = path();
 
     % always backup next to current OSLCONF
     confdir = fileparts(getenv('OSLCONF'));
-    fprintf(1,'[OSL] Backing-up matlab path to: %s\n',confdir);
-
     fname = fullfile( confdir, '.path-backup.tmp' );
+
+    fprintf(1,'[OSL] Backing-up matlab path to: %s\n', fname);
+
     setenv('OSL_PATH_BACKUP', fname);
     fh = fopen(fname,'w+');
     fwrite(fh,p);
